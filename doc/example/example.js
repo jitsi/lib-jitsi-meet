ba5275ef2f973f29ccc81a08ddb9b14d13791427
hristoterezov <hristo@jitsi.org>
2015-12-11 01:15:28 +1100

    Adds follow entity parameter for the recording

--- doc/example/example.js
+++ doc/example/example.js
@@ -42,8 +42,8 @@ function onLocalTracks(tracks)
             $("body").append("<video autoplay='1' id='localVideo" + i + "' />");
             localTracks[i].attach($("#localVideo" + i ));
         } else {
-            // $("body").append("<audio autoplay='1' id='localAudio" + i + "' />");
-            // localTracks[i].attach($("#localAudio" + i ));
+            $("body").append("<audio autoplay='1' muted='true' id='localAudio" + i + "' />");
+            localTracks[i].attach($("#localAudio" + i ));
         }
         if(isJoined)
             room.addTrack(localTracks[i]);
@@ -105,7 +105,7 @@ function onUserLeft(id) {
  * That function is called when connection is established successfully
  */
 function onConnectionSuccess(){
-    room = connection.initJitsiConference("conference6", confOptions);
+    room = connection.initJitsiConference("conference10", confOptions);
     room.on(JitsiMeetJS.events.conference.TRACK_ADDED, onRemoteTrack);
     room.on(JitsiMeetJS.events.conference.TRACK_REMOVED, function (track) {
         console.log("track removed!!!" + track);
