66806b6b2e687f9f2e6a81d594ebd91486931958
damencho <damencho@jitsi.org>
2016-04-25 11:42:14 +0300

    Fixes callstats wrtcFuncNames signalingError.

--- modules/statistics/CallStats.js
+++ modules/statistics/CallStats.js
@@ -15,7 +15,7 @@ var wrtcFuncNames = {
     setRemoteDescription: "setRemoteDescription",
     addIceCandidate:      "addIceCandidate",
     getUserMedia:         "getUserMedia",
-    signallingError:      "signallingError"
+    signalingError:       "signalingError"
 };
 
 /**
@@ -377,7 +377,7 @@ CallStats.sendAddIceCandidateFailed = _try_catch(function (e, pc, cs) {
  * @param {CallStats} cs callstats instance related to the error (optional)
  */
 CallStats.sendUnhandledError = _try_catch(function (e, cs) {
-    CallStats._reportError.call(cs, wrtcFuncNames.signallingError, e, null);
+    CallStats._reportError.call(cs, wrtcFuncNames.signalingError, e, null);
 });
 
 module.exports = CallStats;
