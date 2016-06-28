fa1d6b784b610dd1c316cfaed81dd0d6311faf06
damencho <damencho@jitsi.org>
2016-06-28 10:09:34 -0500

    Adds a variable, avoids duplicate method call.

--- JitsiMediaDevices.js
+++ JitsiMediaDevices.js
@@ -95,8 +95,8 @@ var JitsiMediaDevices = {
      */
     setAudioOutputDevice: function (deviceId) {
 
-        if (RTC.getCurrentlyAvailableMediaDevices()
-            && RTC.getCurrentlyAvailableMediaDevices().length > 0)
+        var availableDevices = RTC.getCurrentlyAvailableMediaDevices();
+        if (availableDevices && availableDevices.length > 0)
         {
             // if we have devices info report device to stats
             // normally this will not happen on startup as this method is called
