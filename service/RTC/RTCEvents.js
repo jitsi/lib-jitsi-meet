af44ba0aa092206a83f62322de7cbee7c8c6ef85
paweldomas <pawel.domas@jitsi.org>
2016-08-24 15:14:20 -0500

    feat(RTCEvents): add REMOTE_TRACK_MUTE/UNMUTE
    Add "onmute" and "onmute" handlers to the WebRTC MediaStream and trigger
    corresponding RTCEvents.

--- service/RTC/RTCEvents.js
+++ service/RTC/RTCEvents.js
@@ -7,6 +7,8 @@ var RTCEvents = {
     LASTN_ENDPOINT_CHANGED: "rtc.lastn_endpoint_changed",
     AVAILABLE_DEVICES_CHANGED: "rtc.available_devices_changed",
     TRACK_ATTACHED: "rtc.track_attached",
+    REMOTE_TRACK_MUTE: "rtc.remote_track_mute",
+    REMOTE_TRACK_UNMUTE: "rtc.remote_track_unmute",
     AUDIO_OUTPUT_DEVICE_CHANGED: "rtc.audio_output_device_changed",
     DEVICE_LIST_CHANGED: "rtc.device_list_changed",
     DEVICE_LIST_AVAILABLE: "rtc.device_list_available",
