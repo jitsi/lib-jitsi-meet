4f10ce43a933e75450741b2c9ea5797509098fe0
Lyubomir Marinov <lyubomir.marinov@jitsi.org>
2016-09-09 05:01:42 -0500

    ES6 for JitsiMediaDevicesEvents

--- JitsiMediaDevicesEvents.js
+++ JitsiMediaDevicesEvents.js
@@ -1,27 +1,24 @@
 /**
- * Enumeration with the events for the media devices.
- * @type {{string: string}}
+ * The events for the media devices.
  */
-var JitsiMediaDevicesEvents = {
-    /**
-     * Indicates that the list of available media devices has been changed. The
-     * event provides the following parameters to its listeners:
-     *
-     * @param {MediaDeviceInfo[]} devices - array of MediaDeviceInfo or
-     *  MediaDeviceInfo-like objects that are currently connected.
-     *  @see https://developer.mozilla.org/en-US/docs/Web/API/MediaDeviceInfo
-     */
-    DEVICE_LIST_CHANGED: "mediaDevices.devicechange",
-    /**
-     * Indicates that the environment is currently showing permission prompt to
-     * access camera and/or microphone. The event provides the following
-     * parameters to its listeners:
-     *
-     * @param {'chrome'|'opera'|'firefox'|'iexplorer'|'safari'|'nwjs'
-     *      |'react-native'|'android'} environmentType - type of browser or
-     *      other execution environment.
-     */
-    PERMISSION_PROMPT_IS_SHOWN: "mediaDevices.permissionPromptIsShown"
-};
 
-module.exports = JitsiMediaDevicesEvents;
+/**
+ * Indicates that the list of available media devices has been changed. The
+ * event provides the following parameters to its listeners:
+ *
+ * @param {MediaDeviceInfo[]} devices - array of MediaDeviceInfo or
+ *  MediaDeviceInfo-like objects that are currently connected.
+ *  @see https://developer.mozilla.org/en-US/docs/Web/API/MediaDeviceInfo
+ */
+export const DEVICE_LIST_CHANGED = "mediaDevices.devicechange";
+/**
+ * Indicates that the environment is currently showing permission prompt to
+ * access camera and/or microphone. The event provides the following
+ * parameters to its listeners:
+ *
+ * @param {'chrome'|'opera'|'firefox'|'iexplorer'|'safari'|'nwjs'
+ *  |'react-native'|'android'} environmentType - type of browser or
+ *  other execution environment.
+ */
+export const PERMISSION_PROMPT_IS_SHOWN
+    = "mediaDevices.permissionPromptIsShown";
