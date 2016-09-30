6858c5715c384a8e3891625b63bfa4767b1b9ef2
hristoterezov <hristo@jitsi.org>
2016-09-30 17:31:03 -0500

    feat(feedback): Tries to send the detailed feedback to analytics

--- modules/statistics/AnalyticsAdapter.js
+++ modules/statistics/AnalyticsAdapter.js
@@ -11,13 +11,56 @@ function AnalyticsAdapter() {
 // in this case we accumulate them in this array and send them on init
 AnalyticsAdapter.eventsQueue = [];
 
-// XXX Since we asynchronously load the integration of the analytics API and the
-// analytics API may asynchronously load its implementation (e.g. Google
-// Analytics), we cannot make the decision with respect to which analytics
-// implementation we will use here and we have to postpone it i.e. we will make
-// a lazy decision, will wait for loaded or dispose methods to be called.
-// in the meantime we accumulate any events received
+/**
+ * Sends analytics event.
+ * @param action
+ * @param data
+ * @param label
+ */
 AnalyticsAdapter.prototype.sendEvent = function (action, data, label) {
+    if(this._checkAnalyticsAndMaybeCacheEvent(
+        "sendEvent", action, data,label)) {
+        try {
+            this.analytics.sendEvent(action, data, label, this.browserName);
+        } catch (ignored) { // eslint-disable-line no-empty
+        }
+    }
+};
+
+/**
+ * Sends feedback.
+ * @param {object} data with proprties:
+ * - {int} overall an integer between 1 and 5 indicating the user feedback
+ * - {string} detailed detailed feedback from the user.
+ * @param label
+ */
+AnalyticsAdapter.prototype.sendFeedback = function (data, label) {
+    if(this._checkAnalyticsAndMaybeCacheEvent(
+        "sendFeedback", null, data,label)) {
+        try {
+            this.analytics.sendFeedback(data, label, this.browserName);
+        } catch (ignored) { // eslint-disable-line no-empty
+        }
+    }
+
+};
+
+/**
+ * Since we asynchronously load the integration of the analytics API and the
+ * analytics API may asynchronously load its implementation (e.g. Google
+ * Analytics), we cannot make the decision with respect to which analytics
+ * implementation we will use here and we have to postpone it i.e. we will make
+ * a lazy decision, will wait for loaded or dispose methods to be called.
+ * in the meantime we accumulate any events received. We should call this
+ * method before trying to send the event.
+ * @param {string} method - Identifies which method should we use later for the
+ * cached events - "sendEvent" or "sendFeedback".
+ * @param action
+ * @param data
+ * @param label
+ */
+AnalyticsAdapter.prototype._checkAnalyticsAndMaybeCacheEvent
+= function (method, action, data, label) {
     if (this.analytics === null || typeof this.analytics === 'undefined') {
         // missing this.analytics but have window implementation, let's use it
         if (window.Analytics) {
@@ -25,19 +68,18 @@ AnalyticsAdapter.prototype.sendEvent = function (action, data, label) {
         }
         else {
             AnalyticsAdapter.eventsQueue.push({
+                method: method,
                 action: action,
                 data: data,
                 label: label
             });
             // stored, lets break here
-            return;
+            return false;
         }
     }
-    try {
-        this.analytics.sendEvent(action, data, label, this.browserName);
-    } catch (ignored) { // eslint-disable-line no-empty
-    }
+    return true;
 };
+
 
 /**
  * Dispose analytics. Clears any available queue element and sets
@@ -59,7 +101,15 @@ AnalyticsAdapter.prototype.loaded = function () {
     // new analytics lets send all events if any
     if (AnalyticsAdapter.eventsQueue.length) {
         AnalyticsAdapter.eventsQueue.forEach(function (event) {
-            this.sendEvent(event.action, event.data, event.label);
+            switch(event.method) {
+                case "sendEvent":
+                    this.sendEvent(event.action, event.data, event.label);
+                    break;
+                case "sendFeedback":
+                    this.sendFeedback(event.data, event.label);
+                    break;
+            }
+
         }.bind(this));
         AnalyticsAdapter.eventsQueue.length = 0;
     }
