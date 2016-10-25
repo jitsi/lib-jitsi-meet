2271d2953e88d33b797bb112adc77c461c32a5c7
paweldomas <pawel.domas@jitsi.org>
2016-10-25 13:15:44 -0500

    feat(xmpp): trigger CONNECTION_FAILED when closed unintentionally
    If Strophe drops the connection while not being asked to, it means that
    most likely some serious error has occurred. One currently known case is
    when a BOSH request fails for more than 4 times then the connection is
    dropped without supplying a reason through the Strophe API
    (the connection status simply goes to DISCONNECTED).

--- modules/xmpp/xmpp.js
+++ modules/xmpp/xmpp.js
@@ -146,6 +146,7 @@ export default class XMPP {
         } else if (status === Strophe.Status.DISCONNECTED) {
             // Stop ping interval
             this.connection.ping.stopInterval();
+            const wasIntentionalDisconnect = this.disconnectInProgress;
             const errMsg = msg ? msg : this.lastErrorMsg;
             this.disconnectInProgress = false;
             if (this.anonymousConnectionFailed) {
@@ -157,6 +158,17 @@ export default class XMPP {
                 this.eventEmitter.emit(
                     JitsiConnectionEvents.CONNECTION_FAILED,
                     JitsiConnectionErrors.OTHER_ERROR, errMsg);
+            } else if (!wasIntentionalDisconnect) {
+                // XXX if Strophe drops the connection while not being asked to,
+                // it means that most likely some serious error has occurred.
+                // One currently known case is when a BOSH request fails for
+                // more than 4 times. The connection is dropped without
+                // supplying a reason(error message/event) through the API.
+                logger.error("XMPP connection dropped!");
+                this.eventEmitter.emit(
+                    JitsiConnectionEvents.CONNECTION_FAILED,
+                    JitsiConnectionErrors.OTHER_ERROR,
+                    errMsg ? errMsg : 'connection-dropped-error');
             } else {
                 this.eventEmitter.emit(
                     JitsiConnectionEvents.CONNECTION_DISCONNECTED, errMsg);
