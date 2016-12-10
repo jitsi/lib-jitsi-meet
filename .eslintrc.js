aa1bda86a695e09baaf093abf894b84adb2009f8
brian baldino <bbaldino@gmail.com>
2016-12-09 16:55:30 -0800

    Add karma and jasmine unit testing to lib-jitsi-meet.  also add a very basic example test for SDPUtil

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     'env': {
         'browser': true,
         'commonjs': true,
-        'es6': true
+        'es6': true,
+        'jasmine': true
     },
     'extends': 'eslint:recommended',
     'globals': {
