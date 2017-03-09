0666d3358caa6b1dc99262246824233ce68fb102
hristoterezov <hristo@jitsi.org>
2017-03-09 13:24:42 -0600

    chore(eslint): Add no-dupe-keys rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
         'no-cond-assign': 2,
         'no-constant-condition': 2,
         'no-control-regex': 2,
-        'no-debugger': 2
+        'no-debugger': 2,
+        'no-dupe-args': 2
     }
 };
