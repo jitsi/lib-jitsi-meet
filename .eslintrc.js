62c2e5a954866cc6c651134d5a1aa78ff9c2c009
hristoterezov <hristo@jitsi.org>
2017-03-09 13:26:07 -0600

    chore(eslint): Add no-duplicate-case rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
         'no-constant-condition': 2,
         'no-control-regex': 2,
         'no-debugger': 2,
-        'no-dupe-args': 2
+        'no-dupe-args': 2,
+        'no-duplicate-case': 2
     }
 };
