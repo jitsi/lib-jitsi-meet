12bad1a1da0531d2231dfdc29e2baaa00c6019d6
paweldomas <pawel.domas@jitsi.org>
2017-03-17 15:30:54 -0500

    fix: remove JSUtil.js

--- modules/util/JSUtil.js
+++
@@ -1,8 +0,0 @@
-/**
- * Gets object's values.
- * @param {Object} object
- * @return {Array<Object>}
- */
-export function getValues(object) {
-    return Object.keys(object).map(key => object[key]);
-}
