a5c686d824eaa63eb1a7024221cc68694d0b2503
paweldomas <pawel.domas@jitsi.org>
2017-05-18 09:35:42 -0500

    fix(JitsiConferenceEvents): remove CONNECTION_STATS
    CONNECTION_STATS event is no longer emitted.

--- JitsiConferenceEvents.js
+++ JitsiConferenceEvents.js
@@ -57,13 +57,6 @@ export const CONNECTION_INTERRUPTED = 'conference.connectionInterrupted';
 export const CONNECTION_RESTORED = 'conference.connectionRestored';
 
 /**
- * New local connection statistics are received.
- * @deprecated Use ConnectionQualityEvents.LOCAL_STATS_UPDATED instead.
- * FIXME remove unused
- */
-export const CONNECTION_STATS = 'conference.connectionStats';
-
-/**
  * A user has changed it display name
  */
 export const DISPLAY_NAME_CHANGED = 'conference.displayNameChanged';
