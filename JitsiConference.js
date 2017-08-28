a22cf0979f5e0917b905fbd3740928ebfebda79e
Lyubo Marinov <lmarinov@atlassian.com>
2017-08-28 17:07:50 -0500

    Coding style
    Reduce the source code by replacing this.options.config with config.

--- JitsiConference.js
+++ JitsiConference.js
@@ -204,7 +204,9 @@ JitsiConference.prototype._init = function(options = {}) {
         this.eventManager.setupXMPPListeners();
     }
 
-    this.room = this.xmpp.createRoom(this.options.name, this.options.config);
+    const { config } = this.options;
+
+    this.room = this.xmpp.createRoom(this.options.name, config);
 
     // Connection interrupted/restored listeners
     this._onIceConnectionInterrupted
@@ -236,23 +238,20 @@ JitsiConference.prototype._init = function(options = {}) {
                 // as an entry point through config for tuning up purposes.
                 // Default values should be adjusted as soon as optimal values
                 // are discovered.
-                rtcMuteTimeout:
-                    this.options.config._peerConnStatusRtcMuteTimeout,
-                outOfLastNTimeout:
-                    this.options.config._peerConnStatusOutOfLastNTimeout
+                rtcMuteTimeout: config._peerConnStatusRtcMuteTimeout,
+                outOfLastNTimeout: config._peerConnStatusOutOfLastNTimeout
             });
     this.participantConnectionStatus.init();
 
     if (!this.statistics) {
         this.statistics = new Statistics(this.xmpp, {
-            callStatsID: this.options.config.callStatsID,
-            callStatsSecret: this.options.config.callStatsSecret,
-            callStatsConfIDNamespace:
-                this.options.config.callStatsConfIDNamespace
-                    || window.location.hostname,
-            callStatsCustomScriptUrl:
-                this.options.config.callStatsCustomScriptUrl,
             callStatsAliasName: this.myUserId(),
+            callStatsConfIDNamespace:
+                config.callStatsConfIDNamespace
+                    || window.location.hostname,
+            callStatsCustomScriptUrl: config.callStatsCustomScriptUrl,
+            callStatsID: config.callStatsID,
+            callStatsSecret: config.callStatsSecret,
             roomName: this.options.name
         });
     }
@@ -263,7 +262,7 @@ JitsiConference.prototype._init = function(options = {}) {
     // listeners are removed from statistics module.
     this.eventManager.setupStatisticsListeners();
 
-    if (this.options.config.enableTalkWhileMuted) {
+    if (config.enableTalkWhileMuted) {
         // eslint-disable-next-line no-new
         new TalkMutedDetection(
             this,
@@ -271,8 +270,8 @@ JitsiConference.prototype._init = function(options = {}) {
                 this.eventEmitter.emit(JitsiConferenceEvents.TALK_WHILE_MUTED));
     }
 
-    if ('channelLastN' in options.config) {
-        this.setLastN(options.config.channelLastN);
+    if ('channelLastN' in config) {
+        this.setLastN(config.channelLastN);
     }
 
     /**
