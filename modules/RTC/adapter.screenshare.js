d56bdb9e623b4e2ddfa96827bf5daf4f85cdce6c
Boston Dell-Vandenberg <boston@pomelodesign.com>
2017-08-29 16:27:57 -0400

    Add check for iOS WebView (#572)

--- modules/RTC/adapter.screenshare.js
+++ modules/RTC/adapter.screenshare.js
@@ -373,6 +373,16 @@ AdapterJS.parseWebrtcDetectedBrowser = function () {
     webrtcDetectedType      = isMobile.length === 0 ? 'plugin' : null;
     webrtcDetectedDCSupport = isMobile.length === 0 ? 'SCTP' : null;
 
+  // Detect WebView on iOS (does not support WebRTC yet)
+  } else if (/(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/i.test(navigator.userAgent)) {
+    hasMatch = navigator.userAgent.match(/AppleWebKit\/([0-9]+)\./) || [];
+
+    webrtcDetectedBrowser   = 'safari';
+    webrtcDetectedVersion   = parseInt(hasMatch[1] || '0', 10);
+    webrtcMinimumVersion    = 0;
+    webrtcDetectedType      = null;
+    webrtcDetectedDCSupport = null;
+
   }
 
   window.webrtcDetectedBrowser   = webrtcDetectedBrowser;
