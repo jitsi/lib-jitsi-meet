02a7e4f3433a2b733c3c9119461184db703b5993
Saúl Ibarra Corretgé <saghul@jitsi.org>
2017-12-15 09:29:00 +0100

    feat(JitsiConference): lower log severity if caps couldn't be discovered

--- JitsiConference.js
+++ JitsiConference.js
@@ -1177,7 +1177,7 @@ JitsiConference.prototype.onMemberJoined = function(
             participant._supportsDTMF = features.has('urn:xmpp:jingle:dtmf:0');
             this.updateDTMFSupport();
         },
-        error => logger.error(`Failed to discover features of ${jid}`, error));
+        error => logger.warn(`Failed to discover features of ${jid}`, error));
 
     this._maybeStartOrStopP2P();
 };
