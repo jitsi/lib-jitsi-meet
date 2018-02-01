2267cf3c9890aa3e710bf3f8db5464143fea70b0
hristoterezov <hristo@jitsi.org>
2018-02-01 12:27:34 -0600

    fix(browser_caps): clean up.

--- modules/browser/index.js
+++ modules/browser/index.js
@@ -1,7 +1,4 @@
 import BrowserCapabilities from './BrowserCapabilities';
 
-// Export for browser_capabilities bundle.
-export ExternalBrowserCapabilities from './ExternalBrowserCapabilities';
-
 export * as browsers from './browsers';
 export default new BrowserCapabilities();
