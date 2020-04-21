642b198bbe54ac63981725150dfbe03a8ee33f09
paweldomas <pawel.domas@jitsi.org>
2020-04-21 15:16:05 -0500

    feat(XmppConnection): sendIQ2
    A new method which not only has Promise interface,
    but also allows to queue IQs regardless of the current
    connection's status.
    
    Ping and send ICE failed notification requests are changed
    to use this method which makes it more reliable on XMPP
    Websocket connections on mobile(which in contrary to BOSH
    dies much faster there).

---
+++ modules/xmpp/XmppConnection.spec.js
@@ -0,0 +1,193 @@
+import { default as XmppConnection } from './XmppConnection';
+import { $iq, Strophe } from 'strophe.js';
+import { nextTick } from '../util/TestUtils';
+
+/**
+ * Mock Strophe connection.
+ */
+class MockStropheConnection {
+    /**
+     * XMPP service URL.
+     *
+     * @returns {string}
+     */
+    get service() {
+        return 'wss://localhost/xmpp-websocket';
+    }
+
+    /**
+     * {@see Strophe.Connection.connect}
+     */
+    connect(jid, pass, callback) {
+        this._connectCb = callback;
+    }
+
+    /**
+     * {@see Strophe.Connection.disconnect}
+     */
+    disconnect() {
+        this.simulateConnectionState(Strophe.Status.DISCONNECTING);
+        this.simulateConnectionState(Strophe.Status.DISCONNECTED);
+    }
+
+    /**
+     * Simulates transition to the new connection status.
+     *
+     * @param {Strophe.Status} newState - The new connection status to set.
+     * @returns {void}
+     */
+    simulateConnectionState(newState) {
+        this._connectCb(newState);
+    }
+
+    /**
+     * {@see Strophe.Connection.sendIQ}.
+     */
+    sendIQ(iq, resultCb) {
+        resultCb();
+    }
+}
+
+/**
+ * Creates any IQ.
+ * @returns {Element}
+ */
+function testIQ() {
+    return $iq({
+        to: 'remoteJid',
+        type: 'set'
+    })
+    .c('jingle', { xmlns: 'urn:xmpp:jingle:1',
+        action: 'session-info',
+        initiator: 'blabla',
+        sid: '1234' })
+    .up();
+}
+
+describe('XmppConnection', () => {
+    let connection;
+    let mockStropheConnection;
+    let sendIQSpy;
+
+    beforeEach(() => {
+        jasmine.clock().install();
+
+        spyOn(Strophe, 'Connection').and.callFake((...args) => {
+            mockStropheConnection = new MockStropheConnection(...args);
+
+            return mockStropheConnection;
+        });
+
+        connection = new XmppConnection({
+            serviceUrl: 'wss://localhost/xmpp-websocket'
+        });
+
+        sendIQSpy = spyOn(mockStropheConnection, 'sendIQ').and.callThrough();
+
+        // eslint-disable-next-line no-empty-function
+        connection.connect('jid', undefined, () => { });
+    });
+
+    afterEach(() => {
+        jasmine.clock().uninstall();
+    });
+    describe('sendIQ2', () => {
+        it('will send the IQ immediately if connected', () => {
+            mockStropheConnection.simulateConnectionState(Strophe.Status.CONNECTED);
+
+            return connection.sendIQ2(testIQ(), { timeout: 15000 })
+                .then(() => {
+                    expect(sendIQSpy).toHaveBeenCalled();
+                });
+        });
+        it('will send the IQ on reconnect', () => {
+            mockStropheConnection.simulateConnectionState(Strophe.Status.CONNECTING);
+
+            let resolved = false;
+
+            connection
+                .sendIQ2(testIQ(), { timeout: 15000 })
+                .then(() => {
+                    resolved = true;
+                });
+
+            jasmine.clock().tick(10000);
+
+            return nextTick()
+                .then(() => {
+                    expect(resolved).toBe(false);
+                    expect(sendIQSpy).not.toHaveBeenCalled();
+
+                    mockStropheConnection.simulateConnectionState(Strophe.Status.CONNECTED);
+
+                    return nextTick();
+                })
+                .then(() => {
+                    expect(resolved).toBe(true);
+                    expect(sendIQSpy).toHaveBeenCalled();
+                });
+        });
+        it('will timeout the operation if not connected in time', () => {
+            mockStropheConnection.simulateConnectionState(Strophe.Status.CONNECTING);
+
+            let rejected = false, resolved = false;
+
+            connection
+                .sendIQ2(testIQ(), { timeout: 15000 })
+                .then(() => {
+                    resolved = true;
+                }, () => {
+                    rejected = true;
+                });
+
+            jasmine.clock().tick(10000);
+
+            return nextTick()
+                .then(() => {
+                    expect(sendIQSpy).not.toHaveBeenCalled();
+                    expect(resolved).toBe(false);
+                    expect(rejected).toBe(false);
+
+                    jasmine.clock().tick(10000);
+
+                    return nextTick();
+                })
+                .then(() => {
+                    expect(sendIQSpy).not.toHaveBeenCalled();
+                    expect(resolved).toBe(false);
+                    expect(rejected).toBe(true);
+                });
+        });
+        it('will reject the promise on explicit disconnect', () => {
+            mockStropheConnection.simulateConnectionState(Strophe.Status.CONNECTING);
+
+            let rejected = false, resolved = false;
+
+            connection
+                .sendIQ2(testIQ(), { timeout: 15000 })
+                .then(() => {
+                    resolved = true;
+                }, error => {
+                    rejected = error;
+                });
+
+            jasmine.clock().tick(10000);
+
+            return nextTick()
+                .then(() => {
+                    expect(sendIQSpy).not.toHaveBeenCalled();
+                    expect(resolved).toBe(false);
+                    expect(rejected).toBe(false);
+
+                    connection.disconnect();
+
+                    return nextTick();
+                })
+                .then(() => {
+                    expect(sendIQSpy).not.toHaveBeenCalled();
+                    expect(resolved).toBe(false);
+                    expect(rejected).toEqual(new Error('disconnect'));
+                });
+        });
+    });
+});
