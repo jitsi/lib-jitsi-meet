de071fe7e4f72e742b171d6199f933483b57747b
enrico.schwendig <es@patientus.de>
2020-04-29 12:34:53 +0200

    Deactivate mute in safari

--- modules/browser/BrowserCapabilities.js
+++ modules/browser/BrowserCapabilities.js
@@ -80,7 +80,7 @@ export default class BrowserCapabilities extends BrowserDetection {
      * @returns {boolean}
      */
     isUserInteractionRequiredForUnmute() {
-        return (this.isFirefox() && this.isVersionLessThan('68')) || this.isSafari();
+        return (this.isFirefox() && this.isVersionLessThan('68'));
     }
 
     /**
