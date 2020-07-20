cf584f56e47ed918b196f93b9fff16de70d155ea
paweldomas <pawel.domas@jitsi.org>
2020-07-20 10:32:26 -0500

    fix(StropheLastSuccess): refresh the timestamp in connected
    An 'item-not-found' or other error on Websocket reconnect will refresh
    the timestamp which will give misleading information about when
    the last stanza was received.

--- modules/xmpp/StropheLastSuccess.js
+++ modules/xmpp/StropheLastSuccess.js
@@ -12,13 +12,18 @@ export default class LastRequestTracker {
     /**
      * Starts tracking requests on the given connection.
      *
+     * @param {XmppConnection} xmppConnection - The XMPP connection which manages the given {@code stropheConnection}.
      * @param {Object} stropheConnection - Strophe connection instance.
      */
-    startTracking(stropheConnection) {
+    startTracking(xmppConnection, stropheConnection) {
         const originalRawInput = stropheConnection.rawInput;
 
-        stropheConnection.rawInput = function(...args) {
-            this._lastSuccess = Date.now();
+        stropheConnection.rawInput = (...args) => {
+            // It's okay to use rawInput callback only once the connection has been established, otherwise it will
+            // treat 'item-not-found' or other connection error on websocket reconnect as successful stanza received.
+            if (xmppConnection.connected) {
+                this._lastSuccess = Date.now();
+            }
             originalRawInput.apply(stropheConnection, args);
         };
     }
