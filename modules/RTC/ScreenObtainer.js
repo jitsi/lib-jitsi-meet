2a2405e93614efd43d4db62f51399f5a870a1615
tmoldovan8x8 <62697631+tmoldovan8x8@users.noreply.github.com>
2020-10-29 12:48:25 +0200

    feat(rn) support getDisplayMedia
    Co-authored-by: Saúl Ibarra Corretgé <s@saghul.net>

--- modules/RTC/ScreenObtainer.js
+++ modules/RTC/ScreenObtainer.js
@@ -83,6 +83,8 @@ const ScreenObtainer = {
             };
         } else if (browser.isElectron()) {
             return this.obtainScreenOnElectron;
+        } else if (browser.isReactNative() && browser.supportsGetDisplayMedia()) {
+            return this.obtainScreenFromGetDisplayMediaRN;
         } else if (browser.supportsGetDisplayMedia()) {
             return this.obtainScreenFromGetDisplayMedia;
         }
@@ -206,6 +208,27 @@ const ScreenObtainer = {
                 }
 
                 errorCallback(new JitsiTrackError(JitsiTrackErrors.SCREENSHARING_USER_CANCELED));
+            });
+    },
+
+    /**
+     * Obtains a screen capture stream using getDisplayMedia.
+     *
+     * @param callback - The success callback.
+     * @param errorCallback - The error callback.
+     */
+    obtainScreenFromGetDisplayMediaRN(options, callback, errorCallback) {
+        logger.info('Using getDisplayMedia for screen sharing');
+
+        navigator.mediaDevices.getDisplayMedia({ video: true })
+            .then(stream => {
+                callback({
+                    stream,
+                    sourceId: stream.id });
+            })
+            .catch(() => {
+                errorCallback(new JitsiTrackError(JitsiTrackErrors
+                    .SCREENSHARING_USER_CANCELED));
             });
     }
 };
