4e3c2862903f6adf5b4e9ea0bb653cdc84f2905a
Titus-Andrei Moldovan <titus.moldovan@8x8.com>
2020-10-29 12:41:57 +0200

    addresses code review remarks

--- modules/RTC/RTCUtils.js
+++ modules/RTC/RTCUtils.js
@@ -1147,19 +1147,20 @@ class RTCUtils extends Listenable {
                         + 'not supported on this browser.');
                 }
 
-                return this._newGetDesktopMedia(this._parseDesktopSharingOptions(options))
-                            .then(desktop => {
-                                return {
-                                    audioVideo,
-                                    desktop };
-                            })
-                            .catch(err => {
-                                if (audioVideo) {
-                                    this.stopMediaStream(audioVideo);
-                                }
-
-                                return Promise.reject(err);
-                            });
+                return new Promise((resolve, reject) => {
+                    screenObtainer.obtainStream(
+                        this._parseDesktopSharingOptions(options),
+                        desktop => resolve({
+                            audioVideo,
+                            desktop
+                        }),
+                        error => {
+                            if (audioVideo) {
+                                this.stopMediaStream(audioVideo);
+                            }
+                            reject(error);
+                        });
+                });
             });
     }
 
