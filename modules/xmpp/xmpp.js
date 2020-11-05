c6065a530f4144cebe6138191fe35abe3a53f730
Saúl Ibarra Corretgé <saghul@jitsi.org>
2020-11-05 10:51:36 +0100

    fix(stun) update default list of STUN servers

--- modules/xmpp/xmpp.js
+++ modules/xmpp/xmpp.js
@@ -66,9 +66,7 @@ function initStropheNativePlugins() {
  * A list of ice servers to use by default for P2P.
  */
 export const DEFAULT_STUN_SERVERS = [
-    { urls: 'stun:stun.l.google.com:19302' },
-    { urls: 'stun:stun1.l.google.com:19302' },
-    { urls: 'stun:stun2.l.google.com:19302' }
+    { urls: 'stun:meet-jit-si-turnrelay.jitsi.net:443' }
 ];
 
 /**
