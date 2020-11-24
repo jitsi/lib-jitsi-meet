09d748f221b168c6344ade029e6b07fd3a67e348
Boris Grozev <boris@jitsi.org>
2020-11-24 07:37:52 -0600

    feat: Log the jvb version.

--- modules/RTC/BridgeChannel.js
+++ modules/RTC/BridgeChannel.js
@@ -335,6 +335,10 @@ export default class BridgeChannel {
                 }
                 break;
             }
+            case 'ServerHello': {
+                logger.info(`Received ServerHello, version=${obj.version}.`);
+                break;
+            }
             default: {
                 logger.debug('Channel JSON-formatted message: ', obj);
 
