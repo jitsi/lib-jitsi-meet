5891fa5b79f567a9c76d84ab566573abe2581634
Jaya Allamsetty <jaya.allamsetty@8x8.com>
2020-12-02 15:55:10 -0500

    fix(RTX): Re-enable RTX on Firefox.

--- modules/xmpp/xmpp.js
+++ modules/xmpp/xmpp.js
@@ -196,8 +196,9 @@ export default class XMPP extends Listenable {
         this.caps.addFeature('urn:xmpp:jingle:apps:rtp:audio');
         this.caps.addFeature('urn:xmpp:jingle:apps:rtp:video');
 
-        // Disable RTX on Firefox because of https://bugzilla.mozilla.org/show_bug.cgi?id=1668028.
-        if (!(this.options.disableRtx || browser.isFirefox())) {
+        // Disable RTX on Firefox 83 and older versions because of
+        // https://bugzilla.mozilla.org/show_bug.cgi?id=1668028
+        if (!(this.options.disableRtx || (browser.isFirefox() && browser.isVersionLessThan(84)))) {
             this.caps.addFeature('urn:ietf:rfc:4588');
         }
         if (this.options.enableOpusRed === true && browser.supportsAudioRed()) {
