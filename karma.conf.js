d7cdf8f277ce98afd1939729fce3c66c757cfcbd
Saúl Ibarra Corretgé <saghul@jitsi.org>
2021-02-10 10:58:34 +0100

    fix(example) simplify
    - Drop unneeded bundled libraries
    - Use jQuery from a CDN

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function(config) {
 
         // list of files / patterns to load in the browser
         files: [
-            './doc/example/libs/jquery-2.1.1.js',
+            'https://code.jquery.com/jquery-3.5.1.min.js',
             'node_modules/core-js/index.js',
             './modules/**/*.spec.js'
         ],
