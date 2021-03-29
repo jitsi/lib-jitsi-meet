63d7164b349d06b2e57458215d5f024a400a75b4
Jaya Allamsetty <jaya.allamsetty@8x8.com>
2021-03-29 16:15:26 -0400

    squash: fix typo in comments.

--- modules/statistics/constants.js
+++ modules/statistics/constants.js
@@ -2,8 +2,7 @@ export const CALLSTATS_SCRIPT_URL = 'https://api.callstats.io/static/callstats-w
 
 /**
  * The number of remote speakers for which the audio levels will be calculated using
- * RTCRtpReceiver#getSynchronizationSources. Limit the number of endpoints to save cpu on the client sources.
- * Limit the number of endpoints to save cpu on the client as this API call is known to take longer to execute
- * when there are many audio receivers.
+ * RTCRtpReceiver#getSynchronizationSources. Limit the number of endpoints to save cpu on the client as this API call
+ * is known to take longer to execute when there are many audio receivers.
  */
 export const SPEAKERS_AUDIO_LEVELS = 5;
