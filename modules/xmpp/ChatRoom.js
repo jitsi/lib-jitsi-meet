d73723dae68ccbda83b529ed30c2c3340d9f3e0d
Дамян Минков <damencho@jitsi.org>
2021-04-13 15:53:29 -0500

    Update presence and skip default values (#1536)
    * feat: Video type camera is default value of missing, skip it in presence.
    
    We skip sending initial video type camera if it is already missing from presence, if it changes we send the last value.
    We also are dropping the namespace as it is not used anywhere and just increases presence size.
    
    * feat: A/V muted is default so we can skip adding it initially to the presence.
    
    Dropping the namespace as it is not used anywhere and that reduces size.
    
    * squash: Drops unused setting.
    
    * squash: Adds a config option to enable skipping the muted state.

--- modules/xmpp/ChatRoom.js
+++ modules/xmpp/ChatRoom.js
@@ -161,11 +161,6 @@ export default class ChatRoom extends Listenable {
             });
         }
 
-        // We need to broadcast 'videomuted' status from the beginning, cause
-        // Jicofo makes decisions based on that. Initialize it with 'false'
-        // here.
-        this.addVideoInfoToPresence(false);
-
         if (options.deploymentInfo && options.deploymentInfo.userRegion) {
             this.presMap.nodes.push({
                 'tagName': 'region',
@@ -1419,7 +1414,7 @@ export default class ChatRoom extends Listenable {
      * Adds the key to the presence map, overriding any previous value.
      * @param key The key to add or replace.
      * @param values The new values.
-     * @returns {boolean|null} <tt>true</tt> if the operation succeeded or <tt>false</tt> when no add or replce was
+     * @returns {boolean|null} <tt>true</tt> if the operation succeeded or <tt>false</tt> when no add or replace was
      * performed as the value was already there.
      */
     addOrReplaceInPresence(key, values) {
@@ -1562,10 +1557,16 @@ export default class ChatRoom extends Listenable {
      * @param mute
      */
     addAudioInfoToPresence(mute) {
+        const audioMutedTagName = 'audiomuted';
+
+        // we skip adding it as muted is default value
+        if (mute && !this.getFromPresence(audioMutedTagName) && this.options.testing.enableSkipDefaultMutedState) {
+            return false;
+        }
+
         return this.addOrReplaceInPresence(
-            'audiomuted',
+            audioMutedTagName,
             {
-                attributes: { 'xmlns': 'http://jitsi.org/jitmeet/audio' },
                 value: mute.toString()
             });
     }
@@ -1588,10 +1589,16 @@ export default class ChatRoom extends Listenable {
      * @param mute
      */
     addVideoInfoToPresence(mute) {
+        const videoMutedTagName = 'videomuted';
+
+        // we skip adding it as muted is default value
+        if (mute && !this.getFromPresence(videoMutedTagName) && this.options.testing.enableSkipDefaultMutedState) {
+            return false;
+        }
+
         return this.addOrReplaceInPresence(
-            'videomuted',
+            videoMutedTagName,
             {
-                attributes: { 'xmlns': 'http://jitsi.org/jitmeet/video' },
                 value: mute.toString()
             });
     }
