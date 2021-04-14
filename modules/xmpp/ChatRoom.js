552cd4397586109b15c403e5e9542d4052ad4a9b
damencho <damencho@jitsi.org>
2021-04-14 09:41:36 -0500

    fix: Drops unused config.

--- modules/xmpp/ChatRoom.js
+++ modules/xmpp/ChatRoom.js
@@ -1560,7 +1560,7 @@ export default class ChatRoom extends Listenable {
         const audioMutedTagName = 'audiomuted';
 
         // we skip adding it as muted is default value
-        if (mute && !this.getFromPresence(audioMutedTagName) && this.options.testing.enableSkipDefaultMutedState) {
+        if (mute && !this.getFromPresence(audioMutedTagName)) {
             return false;
         }
 
@@ -1592,7 +1592,7 @@ export default class ChatRoom extends Listenable {
         const videoMutedTagName = 'videomuted';
 
         // we skip adding it as muted is default value
-        if (mute && !this.getFromPresence(videoMutedTagName) && this.options.testing.enableSkipDefaultMutedState) {
+        if (mute && !this.getFromPresence(videoMutedTagName)) {
             return false;
         }
 
