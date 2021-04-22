895c7eb327c8ac4ea88fe1ca5f24321b36256b92
Jaya Allamsetty <jaya.allamsetty@8x8.com>
2021-04-22 17:26:15 -0400

    fix(RTC): Specify default width of 1280px for video. Fixes https://github.com/jitsi/lib-jitsi-meet/issues/1571.

--- modules/RTC/RTCUtils.js
+++ modules/RTC/RTCUtils.js
@@ -62,7 +62,12 @@ const DEFAULT_CONSTRAINTS = {
         height: {
             ideal: 720,
             max: 720,
-            min: 240
+            min: 180
+        },
+        width: {
+            ideal: 1280,
+            max: 1280,
+            min: 320
         }
     }
 };
