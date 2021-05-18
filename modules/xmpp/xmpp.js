60c566795718ad4fe28f31113479ee8bb3c78e5f
Jaya Allamsetty <jaya.allamsetty@8x8.com>
2021-05-18 15:18:56 -0400

    fix(caps): Disable TCC on Firefox. There is a known issue with Firefox where the BWE gets halved on every renegotiation causing the low upload bitrates from the Firefox clients.

--- modules/xmpp/xmpp.js
+++ modules/xmpp/xmpp.js
@@ -219,7 +219,9 @@ export default class XMPP extends Listenable {
         if (typeof this.options.enableRemb === 'undefined' || this.options.enableRemb) {
             this.caps.addFeature('http://jitsi.org/remb');
         }
-        if (typeof this.options.enableTcc === 'undefined' || this.options.enableTcc) {
+
+        // Disable TCC on Firefox because of a known issue where BWE is halved on every renegotiation.
+        if (!browser.isFirefox() && (typeof this.options.enableTcc === 'undefined' || this.options.enableTcc)) {
             this.caps.addFeature('http://jitsi.org/tcc');
         }
 
