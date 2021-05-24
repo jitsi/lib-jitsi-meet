dc93cdd49947698c48f397b19994d2f831d5ac4c
Jaya Allamsetty <jaya.allamsetty@8x8.com>
2021-05-24 11:17:08 -0400

    fix(SDP): Drop SSRCs whenever the transceiver direction is 'inactive' or 'recvonly'. This is needed only for JVB connections. Add unit tests for LocalSdpMunger.

---
+++ modules/sdp/LocalSdpMunger.spec.js
@@ -0,0 +1,80 @@
+
+import * as transform from 'sdp-transform';
+
+import LocalSdpMunger from './LocalSdpMunger';
+import { default as SampleSdpStrings } from './SampleSdpStrings.js';
+
+/**
+ * Returns the associated ssrc lines for a given media type.
+ *
+ * @param {RTCSessionDescription} desc
+ * @param {string} mediaType
+ * @returns
+ */
+function getSsrcLines(desc, mediaType) {
+    const mline = desc.media.find(m => m.type === mediaType);
+
+    return mline.ssrcs ?? [];
+}
+
+describe('TransformRecvOnly', () => {
+    let localSdpMunger;
+    const tpc = { id: '1' };
+    const localEndpointId = 'sRdpsdg';
+
+    beforeEach(() => {
+        localSdpMunger = new LocalSdpMunger(tpc, localEndpointId);
+    });
+    describe('stripSsrcs', () => {
+        beforeEach(() => { }); // eslint-disable-line no-empty-function
+        it('should strip ssrcs from an sdp with no msid', () => {
+            const sdpStr = transform.write(SampleSdpStrings.recvOnlySdp);
+            const desc = new RTCSessionDescription({
+                type: 'offer',
+                sdp: sdpStr
+            });
+            const transformedDesc = localSdpMunger.transformStreamIdentifiers(desc);
+            const newSdp = transform.parse(transformedDesc.sdp);
+            const audioSsrcs = getSsrcLines(newSdp, 'audio');
+            const videoSsrcs = getSsrcLines(newSdp, 'video');
+
+            expect(audioSsrcs.length).toEqual(0);
+            expect(videoSsrcs.length).toEqual(0);
+        });
+
+        it('should do nothing to an sdp with msid', () => {
+            const sdpStr = transform.write(SampleSdpStrings.simulcastSdp);
+            const desc = new RTCSessionDescription({
+                type: 'offer',
+                sdp: sdpStr
+            });
+            const transformedDesc = localSdpMunger.transformStreamIdentifiers(desc);
+            const newSdp = transform.parse(transformedDesc.sdp);
+            const audioSsrcs = getSsrcLines(newSdp, 'audio');
+            const videoSsrcs = getSsrcLines(newSdp, 'video');
+
+            expect(audioSsrcs.length).toEqual(4);
+            expect(videoSsrcs.length).toEqual(6);
+        });
+
+        it('should add endpointId to msid', () => {
+            const sdpStr = transform.write(SampleSdpStrings.firefoxSdp);
+            const desc = new RTCSessionDescription({
+                type: 'offer',
+                sdp: sdpStr
+            });
+            const transformedDesc = localSdpMunger.transformStreamIdentifiers(desc);
+            const newSdp = transform.parse(transformedDesc.sdp);
+
+            const videoSsrcs = getSsrcLines(newSdp, 'video');
+
+            for (const ssrcLine of videoSsrcs) {
+                if (ssrcLine.attribute === 'msid') {
+                    const msid = ssrcLine.value.split(' ')[0];
+
+                    expect(msid).toBe(`${localEndpointId}-video-${tpc.id}`);
+                }
+            }
+        });
+    });
+});
--- modules/sdp/SampleSdpStrings.js
+++ modules/sdp/SampleSdpStrings.js
@@ -269,6 +269,96 @@ const flexFecVideoMLineSdp = ''
 + 'a=mid:data\r\n'
 + 'a=sctpmap:5000 webrtc-datachannel 1024\r\n';
 
+const recvOnlyAudioMline = ''
++ 'm=audio 54405 RTP/SAVPF 111 103 104 126\r\n'
++ 'c=IN IP4 172.29.32.39\r\n'
++ 'a=rtpmap:111 opus/48000/2\r\n'
++ 'a=rtpmap:103 ISAC/16000\r\n'
++ 'a=rtpmap:104 ISAC/32000\r\n'
++ 'a=rtpmap:126 telephone-event/8000\r\n'
++ 'a=fmtp:111 minptime=10;useinbandfec=1\r\n'
++ 'a=rtcp:9 IN IP4 0.0.0.0\r\n'
++ 'a=extmap:1 urn:ietf:params:rtp-hdrext:ssrc-audio-level\r\n'
++ 'a=extmap:3 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time\r\n'
++ 'a=setup:passive\r\n'
++ 'a=mid:audio\r\n'
++ 'a=recvonly\r\n'
++ 'a=ice-ufrag:adPg\r\n'
++ 'a=ice-pwd:Xsr05Mq8S7CR44DAnusZE26F\r\n'
++ 'a=fingerprint:sha-256 6A:39:DE:11:24:AD:2E:4E:63:D6:69:D3:85:05:53:C7:3C:38:A4:B7:91:74:C0:91:44:FC:94:63:7F:01:AB:A9\r\n'
++ 'a=candidate:1581043602 1 udp 2122260223 172.29.32.39 54405 typ host generation 0\r\n'
++ 'a=ssrc:124723944 cname:peDGrDD6WsxUOki\r\n'
++ 'a=rtcp-mux\r\n';
+
+const recvOnlyVideoMline = ''
++ 'm=video 9 RTP/SAVPF 96 97 98 99 102 121 127 120\r\n'
++ 'c=IN IP4 0.0.0.0\r\n'
++ 'a=rtpmap:96 VP8/90000\r\n'
++ 'a=rtpmap:97 rtx/90000\r\n'
++ 'a=rtpmap:98 VP9/90000\r\n'
++ 'a=rtpmap:99 rtx/90000\r\n'
++ 'a=rtpmap:102 H264/90000\r\n'
++ 'a=rtpmap:121 rtx/90000\r\n'
++ 'a=rtpmap:127 H264/90000\r\n'
++ 'a=rtpmap:120 rtx/90000\r\n'
++ 'a=rtcp:9 IN IP4 0.0.0.0\r\n'
++ 'a=rtcp-fb:96 ccm fir\r\n'
++ 'a=rtcp-fb:96 transport-cc\r\n'
++ 'a=rtcp-fb:96 nack\r\n'
++ 'a=rtcp-fb:96 nack pli\r\n'
++ 'a=rtcp-fb:96 goog-remb\r\n'
++ 'a=rtcp-fb:98 ccm fir\r\n'
++ 'a=rtcp-fb:98 transport-cc\r\n'
++ 'a=rtcp-fb:98 nack\r\n'
++ 'a=rtcp-fb:98 nack pli\r\n'
++ 'a=rtcp-fb:98 goog-remb\r\n'
++ 'a=rtcp-fb:102 ccm fir\r\n'
++ 'a=rtcp-fb:102 transport-cc\r\n'
++ 'a=rtcp-fb:102 nack\r\n'
++ 'a=rtcp-fb:102 nack pli\r\n'
++ 'a=rtcp-fb:102 goog-remb\r\n'
++ 'a=rtcp-fb:127 ccm fir\r\n'
++ 'a=rtcp-fb:127 transport-cc\r\n'
++ 'a=rtcp-fb:127 nack\r\n'
++ 'a=rtcp-fb:127 nack pli\r\n'
++ 'a=rtcp-fb:127 goog-remb\r\n'
++ 'a=fmtp:97 apt=96\r\n'
++ 'a=fmtp:98 profile-id=0\r\n'
++ 'a=fmtp:102 profile-level-id=42e01f;level-asymmetry-allowed=1;packetization-mode=1\r\n'
++ 'a=fmtp:121 apt=102\r\n'
++ 'a=fmtp:127 profile-level-id=42e01f;level-asymmetry-allowed=1:packetization-mode=0\r\n'
++ 'a=fmtp:120 apt=127\r\n'
++ 'a=extmap:3 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time\r\n'
++ 'a=setup:passive\r\n'
++ 'a=mid:video\r\n'
++ 'a=recvonly\r\n'
++ 'a=ice-ufrag:adPg\r\n'
++ 'a=ice-pwd:Xsr05Mq8S7CR44DAnusZE26F\r\n'
++ 'a=fingerprint:sha-256 6A:39:DE:11:24:AD:2E:4E:63:D6:69:D3:85:05:53:C7:3C:38:A4:B7:91:74:C0:91:44:FC:94:63:7F:01:AB:A9\r\n'
++ 'a=ssrc:1757014965 cname:peDGrDD6WsxUOki\r\n'
++ 'a=rtcp-mux\r\n';
+
+const videoMlineFF = ''
++ 'm=video 9 RTP/SAVPF 100 96\r\n'
++ 'c=IN IP4 0.0.0.0\r\n'
++ 'a=rtpmap:100 VP8/90000\r\n'
++ 'a=fmtp:96 apt=100\r\n'
++ 'a=rtcp:9 IN IP4 0.0.0.0\r\n'
++ 'a=rtcp-fb:100 ccm fir\r\n'
++ 'a=rtcp-fb:100 nack\r\n'
++ 'a=rtcp-fb:100 nack pli\r\n'
++ 'a=rtcp-fb:100 goog-remb\r\n'
++ 'a=extmap:3 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time\r\n'
++ 'a=setup:passive\r\n'
++ 'a=mid:video\r\n'
++ 'a=sendrecv\r\n'
++ 'a=ice-ufrag:adPg\r\n'
++ 'a=ice-pwd:Xsr05Mq8S7CR44DAnusZE26F\r\n'
++ 'a=fingerprint:sha-256 6A:39:DE:11:24:AD:2E:4E:63:D6:69:D3:85:05:53:C7:3C:38:A4:B7:91:74:C0:91:44:FC:94:63:7F:01:AB:A9\r\n'
++ 'a=ssrc:984899560 msid:- bdbd2c0a-7959-4578-8db5-9a6a1aec4ecf\r\n'
++ 'a=ssrc:984899560 cname:peDGrDD6WsxUOki/\r\n'
++ 'a=rtcp-mux\r\n';
+
 // A full sdp string representing a client doing simulcast
 const simulcastSdpStr = baseSessionSdp + baseAudioMLineSdp + simulcastVideoMLineSdp + baseDataMLineSdp;
 
@@ -286,6 +376,11 @@ const multiCodecVideoSdpStr = baseSessionSdp + baseAudioMLineSdp + multiCodecVid
 
 // A full sdp string representing a client doing a single video stream with flexfec
 const flexFecSdpStr = baseSessionSdp + baseAudioMLineSdp + flexFecVideoMLineSdp + baseDataMLineSdp;
+
+// A full sdp string representing a client that doesn't have local sources added on Firefox.
+const recvOnlySdpStr = baseSessionSdp + recvOnlyAudioMline + recvOnlyVideoMline;
+
+const sdpFirefoxStr = baseSessionSdp + baseAudioMLineSdp + videoMlineFF;
 
 export default {
     get simulcastSdp() {
@@ -310,6 +405,14 @@ export default {
 
     get flexFecSdp() {
         return transform.parse(flexFecSdpStr);
+    },
+
+    get recvOnlySdp() {
+        return transform.parse(recvOnlySdpStr);
+    },
+
+    get firefoxSdp() {
+        return transform.parse(sdpFirefoxStr);
     }
 };
 
