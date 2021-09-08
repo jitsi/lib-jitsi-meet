fdb74a809b081d38c61cb12d49da22fff102b7a1
titus.moldovan <titus.moldovan@8x8.com>
2021-09-08 09:14:53 +0300

    adds participant joined

--- modules/e2ee/ManualKeyAdapter.js
+++ modules/e2ee/ManualKeyAdapter.js
@@ -8,6 +8,9 @@ export class ManualKeyAdapter extends Listenable {
         this._conf = conference;
         this._key = undefined;
         this._keyIndex = -1;
+
+        this._conf.on(JitsiConferenceEvents.PARTICIPANT_PROPERTY_CHANGED,
+            this._onParticipantPropertyChanged.bind(this));
     }
 
     async initSessions() {
@@ -35,4 +38,31 @@ export class ManualKeyAdapter extends Listenable {
 
         return this._keyIndex;
     }
+
+      /**
+    * Handles an update in a participant's presence property.
+    *
+    * @param {JitsiParticipant} participant - The participant.
+    * @param {string} name - The name of the property that changed.
+    * @param {*} oldValue - The property's previous value.
+    * @param {*} newValue - The property's new value.
+    * @private
+    */
+   async _onParticipantPropertyChanged(participant, name, oldValue, newValue) {
+    switch (name) {
+    case 'e2ee.enabled':
+        if (newValue && this._conf.isE2EEEnabled()) {
+            const participantFeatures = await participant.getFeatures();
+
+            if (participantFeatures.has(FEATURE_E2EE)) {
+                this.eventEmitter.emit(
+                    E2EEncryption.keyAdapterEvents.PARTICIPANT_KEY_UPDATED, 
+                    participant.getId(), 
+                    this._key, 
+                    this._keyIndex);
+            }
+        }
+        break;
+    }
+}
 }
