60349386a50bcb85d6dbcc288c2c5332e5931a68
Hristo Terezov <hristo@jitsi.org>
2021-10-21 15:34:21 -0500

    faet(SDP): Add test for jingle JSON format.

--- modules/sdp/SDP.spec.js
+++ modules/sdp/SDP.spec.js
@@ -2,6 +2,7 @@
 import { $iq } from 'strophe.js';
 
 import FeatureFlags from '../flags/FeatureFlags';
+import { expandSourcesFromJson } from '../xmpp/JingleHelperFunctions';
 
 import SDP from './SDP';
 
@@ -266,6 +267,165 @@ a=ssrc:3758540092 mslabel:mixedmslabel
 
         it('gets converted to SDP', () => {
             const offer = createStanzaElement(stanza);
+            const sdp = new SDP('');
+
+            sdp.fromJingle($(offer).find('>jingle'));
+            const rawSDP = sdp.raw.replace(/o=- \d+/, 'o=- 123'); // replace generated o= timestamp.
+
+            expect(rawSDP).toEqual(expectedSDP);
+        });
+    });
+
+    describe('fromJingleWithJSONFormat', () => {
+        /* eslint-disable max-len*/
+        const stanza = `
+    <iq>
+        <jingle xmlns="urn:xmpp:jingle:1" action="session-initiate" initiator="focus" sid="123">
+            <content name="audio" creator="initiator" senders="both">
+                <description xmlns="urn:xmpp:jingle:apps:rtp:1" maxptime="60" media="audio">
+                    <payload-type name="opus" clockrate="48000" id="111" channels="2">
+                        <parameter value="10" name="minptime"/>
+                        <parameter value="1" name="useinbandfec"/>
+                        <rtcp-fb xmlns="urn:xmpp:jingle:apps:rtp:rtcp-fb:0" type="transport-cc"/>
+                    </payload-type>
+                    <payload-type name="ISAC" clockrate="16000" id="103"/>
+                    <payload-type name="ISAC" clockrate="32000" id="104"/>
+                    <payload-type name="telephone-event" clockrate="8000" id="126"/>
+                    <rtp-hdrext xmlns="urn:xmpp:jingle:apps:rtp:rtp-hdrext:0" id="1" uri="urn:ietf:params:rtp-hdrext:ssrc-audio-level"/>
+                    <rtp-hdrext xmlns="urn:xmpp:jingle:apps:rtp:rtp-hdrext:0" id="5" uri="http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01"/>
+                    <rtcp-mux/>
+                </description>
+                <transport ufrag='someufrag' pwd='somepwd' xmlns='urn:xmpp:jingle:transports:ice-udp:1'>
+                    <fingerprint hash='sha-256' required='false' setup='actpass' xmlns='urn:xmpp:jingle:apps:dtls:0'>09:B1:51:0F:85:4C:80:19:A1:AF:81:73:47:EE:ED:3D:00:3A:84:C7:76:C1:4E:34:BE:56:F6:42:AD:15:D5:D7</fingerprint>
+                    <candidate foundation='1' id='3cbe5aea5bde0c1401a60bbc2' network='0' protocol='udp' generation='0' port='10000' priority='2130706431' type='host' ip='10.0.0.1' component='1'/>
+                    <candidate rel-addr='10.0.0.1' network='0' foundation='2' id='dfcfd075bde0c140ffffffff927646ba' port='10000' protocol='udp' generation='0' rel-port='10000' priority='1694498815' type='srflx' ip='10.0.0.2' component='1'/>
+                </transport>
+            </content>
+            <content name="video" creator="initiator" senders="both">
+                <description xmlns="urn:xmpp:jingle:apps:rtp:1" media="video">
+                    <payload-type name="VP8" clockrate="90000" id="100">
+                        <rtcp-fb xmlns="urn:xmpp:jingle:apps:rtp:rtcp-fb:0" subtype="fir" type="ccm"/>
+                        <rtcp-fb xmlns="urn:xmpp:jingle:apps:rtp:rtcp-fb:0" type="nack"/>
+                        <rtcp-fb xmlns="urn:xmpp:jingle:apps:rtp:rtcp-fb:0" subtype="pli" type="nack"/>
+                        <parameter value="800" name="x-google-start-bitrate"/>
+                        <rtcp-fb xmlns="urn:xmpp:jingle:apps:rtp:rtcp-fb:0" type="transport-cc"/>
+                    </payload-type>
+                    <payload-type name="rtx" clockrate="90000" id="96">
+                        <parameter value="100" name="apt"/>
+                        <rtcp-fb xmlns="urn:xmpp:jingle:apps:rtp:rtcp-fb:0" subtype="fir" type="ccm"/>
+                        <rtcp-fb xmlns="urn:xmpp:jingle:apps:rtp:rtcp-fb:0" type="nack"/>
+                        <rtcp-fb xmlns="urn:xmpp:jingle:apps:rtp:rtcp-fb:0" subtype="pli" type="nack"/>
+                    </payload-type>
+                    <rtp-hdrext xmlns="urn:xmpp:jingle:apps:rtp:rtp-hdrext:0" id="3" uri="http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time"/>
+                    <rtp-hdrext xmlns="urn:xmpp:jingle:apps:rtp:rtp-hdrext:0" id="5" uri="http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01"/>
+                    <rtcp-mux/>
+                </description>
+                <transport ufrag='someufrag' pwd='somepwd' xmlns='urn:xmpp:jingle:transports:ice-udp:1'>
+                    <fingerprint hash='sha-256' required='false' setup='actpass' xmlns='urn:xmpp:jingle:apps:dtls:0'>09:B1:51:0F:85:4C:80:19:A1:AF:81:73:47:EE:ED:3D:00:3A:84:C7:76:C1:4E:34:BE:56:F6:42:AD:15:D5:D7</fingerprint>
+                    <candidate foundation='1' id='3cbe5aea5bde0c1401a60bbc2' network='0' protocol='udp' generation='0' port='10000' priority='2130706431' type='host' ip='10.0.0.1' component='1'/>
+                    <candidate rel-addr='10.0.0.1' network='0' foundation='2' id='dfcfd075bde0c140ffffffff927646ba' port='10000' protocol='udp' generation='0' rel-port='10000' priority='1694498815' type='srflx' ip='10.0.0.2' component='1'/>
+                </transport>
+            </content>
+            <group xmlns="urn:xmpp:jingle:apps:grouping:0" semantics="BUNDLE">
+                <content name="audio"/>
+                <content name="video"/>
+            </group>
+            <json-message xmlns="http://jitsi.org/jitmeet">{"sources":{"831de82b":[[{"s":257838819,"m":"831de82b-video-1 9bb949d4-5abd-4498-98e9-2be1222b8d3e-1"},{"s":865670341,"m":"831de82b-video-1 9bb949d4-5abd-4498-98e9-2be1222b8d3e-1"},{"s":3041289080,"m":"831de82b-video-1 9bb949d4-5abd-4498-98e9-2be1222b8d3e-1"},{"s":6437989,"m":"831de82b-video-1 9bb949d4-5abd-4498-98e9-2be1222b8d3e-1"},{"s":2417192010,"m":"831de82b-video-1 9bb949d4-5abd-4498-98e9-2be1222b8d3e-1"},{"s":3368859313,"m":"831de82b-video-1 9bb949d4-5abd-4498-98e9-2be1222b8d3e-1"}],[["f",257838819,865670341],["s",257838819,3041289080,6437989],["f",3041289080,2417192010],["f",6437989,3368859313]],[]],"07af8d49":[[{"s":110279275,"m":"07af8d49-video-2 f685aa25-0318-442e-bd00-cd2a911236da-2"},{"s":527738645,"m":"07af8d49-video-2 f685aa25-0318-442e-bd00-cd2a911236da-2"},{"s":1201074111,"m":"07af8d49-video-2 f685aa25-0318-442e-bd00-cd2a911236da-2"},{"s":1635907749,"m":"07af8d49-video-2 f685aa25-0318-442e-bd00-cd2a911236da-2"},{"s":3873826414,"m":"07af8d49-video-2 f685aa25-0318-442e-bd00-cd2a911236da-2"},{"s":4101906340,"m":"07af8d49-video-2 f685aa25-0318-442e-bd00-cd2a911236da-2"}],[["f",3873826414,110279275],["s",3873826414,1201074111,1635907749],["f",1201074111,4101906340],["f",1635907749,527738645]],[]],"95edea8d":[[{"s":620660772,"m":"95edea8d-video-1 0c5d94d1-1902-4fb7-bf6a-76517d065d02-1"},{"s":2212130687,"m":"95edea8d-video-1 0c5d94d1-1902-4fb7-bf6a-76517d065d02-1"},{"s":2306112481,"m":"95edea8d-video-1 0c5d94d1-1902-4fb7-bf6a-76517d065d02-1"},{"s":3334993162,"m":"95edea8d-video-1 0c5d94d1-1902-4fb7-bf6a-76517d065d02-1"},{"s":3473290740,"m":"95edea8d-video-1 0c5d94d1-1902-4fb7-bf6a-76517d065d02-1"},{"s":4085804879,"m":"95edea8d-video-1 0c5d94d1-1902-4fb7-bf6a-76517d065d02-1"}],[["f",2306112481,620660772],["s",2306112481,3334993162,4085804879],["f",3334993162,3473290740],["f",4085804879,2212130687]],[]],"jvb":[[{"s":1427774514,"m":"mixedmslabel mixedlabelvideo0","c":"mixed"}],[],[{"s":3659539811,"m":"mixedmslabel mixedlabelaudio0","c":"mixed"}]]}}</json-message>
+        </jingle>
+    </iq>`;
+        const expectedSDP = `v=0
+o=- 123 2 IN IP4 0.0.0.0
+s=-
+t=0 0
+a=group:BUNDLE audio video
+m=audio 1 RTP/SAVPF 111 103 104 126
+c=IN IP4 0.0.0.0
+a=rtcp:1 IN IP4 0.0.0.0
+a=ice-ufrag:someufrag
+a=ice-pwd:somepwd
+a=fingerprint:sha-256 09:B1:51:0F:85:4C:80:19:A1:AF:81:73:47:EE:ED:3D:00:3A:84:C7:76:C1:4E:34:BE:56:F6:42:AD:15:D5:D7
+a=setup:actpass
+a=candidate:1 1 udp 2130706431 10.0.0.1 10000 typ host generation 0
+a=candidate:2 1 udp 1694498815 10.0.0.2 10000 typ srflx raddr 10.0.0.1 rport 10000 generation 0
+a=sendrecv
+a=mid:audio
+a=rtcp-mux
+a=rtpmap:111 opus/48000/2
+a=fmtp:111 minptime=10; useinbandfec=1
+a=rtcp-fb:111 transport-cc
+a=rtpmap:103 ISAC/16000
+a=rtpmap:104 ISAC/32000
+a=rtpmap:126 telephone-event/8000
+a=extmap:1 urn:ietf:params:rtp-hdrext:ssrc-audio-level
+a=extmap:5 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01
+a=ssrc:3659539811 msid:mixedmslabel mixedlabelaudio0
+m=video 1 RTP/SAVPF 100 96
+c=IN IP4 0.0.0.0
+a=rtcp:1 IN IP4 0.0.0.0
+a=ice-ufrag:someufrag
+a=ice-pwd:somepwd
+a=fingerprint:sha-256 09:B1:51:0F:85:4C:80:19:A1:AF:81:73:47:EE:ED:3D:00:3A:84:C7:76:C1:4E:34:BE:56:F6:42:AD:15:D5:D7
+a=setup:actpass
+a=candidate:1 1 udp 2130706431 10.0.0.1 10000 typ host generation 0
+a=candidate:2 1 udp 1694498815 10.0.0.2 10000 typ srflx raddr 10.0.0.1 rport 10000 generation 0
+a=sendrecv
+a=mid:video
+a=rtcp-mux
+a=rtpmap:100 VP8/90000
+a=fmtp:100 x-google-start-bitrate=800
+a=rtcp-fb:100 ccm fir
+a=rtcp-fb:100 nack
+a=rtcp-fb:100 nack pli
+a=rtcp-fb:100 transport-cc
+a=rtpmap:96 rtx/90000
+a=fmtp:96 apt=100
+a=rtcp-fb:96 ccm fir
+a=rtcp-fb:96 nack
+a=rtcp-fb:96 nack pli
+a=extmap:3 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time
+a=extmap:5 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01
+a=ssrc-group:FID 257838819 865670341
+a=ssrc-group:SIM 257838819 3041289080 6437989
+a=ssrc-group:FID 3041289080 2417192010
+a=ssrc-group:FID 6437989 3368859313
+a=ssrc-group:FID 3873826414 110279275
+a=ssrc-group:SIM 3873826414 1201074111 1635907749
+a=ssrc-group:FID 1201074111 4101906340
+a=ssrc-group:FID 1635907749 527738645
+a=ssrc-group:FID 2306112481 620660772
+a=ssrc-group:SIM 2306112481 3334993162 4085804879
+a=ssrc-group:FID 3334993162 3473290740
+a=ssrc-group:FID 4085804879 2212130687
+a=ssrc:1427774514 msid:mixedmslabel mixedlabelvideo0
+a=ssrc:257838819 msid:831de82b-video-1 9bb949d4-5abd-4498-98e9-2be1222b8d3e-1
+a=ssrc:865670341 msid:831de82b-video-1 9bb949d4-5abd-4498-98e9-2be1222b8d3e-1
+a=ssrc:3041289080 msid:831de82b-video-1 9bb949d4-5abd-4498-98e9-2be1222b8d3e-1
+a=ssrc:6437989 msid:831de82b-video-1 9bb949d4-5abd-4498-98e9-2be1222b8d3e-1
+a=ssrc:2417192010 msid:831de82b-video-1 9bb949d4-5abd-4498-98e9-2be1222b8d3e-1
+a=ssrc:3368859313 msid:831de82b-video-1 9bb949d4-5abd-4498-98e9-2be1222b8d3e-1
+a=ssrc:110279275 msid:07af8d49-video-2 f685aa25-0318-442e-bd00-cd2a911236da-2
+a=ssrc:527738645 msid:07af8d49-video-2 f685aa25-0318-442e-bd00-cd2a911236da-2
+a=ssrc:1201074111 msid:07af8d49-video-2 f685aa25-0318-442e-bd00-cd2a911236da-2
+a=ssrc:1635907749 msid:07af8d49-video-2 f685aa25-0318-442e-bd00-cd2a911236da-2
+a=ssrc:3873826414 msid:07af8d49-video-2 f685aa25-0318-442e-bd00-cd2a911236da-2
+a=ssrc:4101906340 msid:07af8d49-video-2 f685aa25-0318-442e-bd00-cd2a911236da-2
+a=ssrc:620660772 msid:95edea8d-video-1 0c5d94d1-1902-4fb7-bf6a-76517d065d02-1
+a=ssrc:2212130687 msid:95edea8d-video-1 0c5d94d1-1902-4fb7-bf6a-76517d065d02-1
+a=ssrc:2306112481 msid:95edea8d-video-1 0c5d94d1-1902-4fb7-bf6a-76517d065d02-1
+a=ssrc:3334993162 msid:95edea8d-video-1 0c5d94d1-1902-4fb7-bf6a-76517d065d02-1
+a=ssrc:3473290740 msid:95edea8d-video-1 0c5d94d1-1902-4fb7-bf6a-76517d065d02-1
+a=ssrc:4085804879 msid:95edea8d-video-1 0c5d94d1-1902-4fb7-bf6a-76517d065d02-1
+`.split('\n').join('\r\n');
+        /* eslint-enable max-len*/
+
+        it('gets converted to SDP', () => {
+            const offer = createStanzaElement(stanza);
+            const jsonMessages = $(offer).find('jingle>json-message');
+
+            for (let i = 0; i < jsonMessages.length; i++) {
+                expandSourcesFromJson(offer, jsonMessages[i]);
+            }
+
             const sdp = new SDP('');
 
             sdp.fromJingle($(offer).find('>jingle'));
---
+++ modules/xmpp/JingleHelperFunctions.js
@@ -0,0 +1,187 @@
+/* global $, $build, __filename */
+
+import { getLogger } from 'jitsi-meet-logger';
+
+import * as MediaType from '../../service/RTC/MediaType';
+import FeatureFlags from '../flags/FeatureFlags';
+
+const logger = getLogger(__filename);
+
+/**
+ * Creates a "source" XML element for the source described in compact JSON format in [sourceCompactJson].
+ * @param {*} owner the endpoint ID of the owner of the source.
+ * @param {*} sourceCompactJson the compact JSON representation of the source.
+ * @returns the created "source" XML element.
+ */
+function _createSourceExtension(owner, sourceCompactJson) {
+    const node = $build('source', {
+        xmlns: 'urn:xmpp:jingle:apps:rtp:ssma:0',
+        ssrc: sourceCompactJson.s,
+        name: FeatureFlags.isSourceNameSignalingEnabled() ? sourceCompactJson.n : undefined
+    });
+
+    if (sourceCompactJson.m) {
+        node.c('parameter', {
+            name: 'msid',
+            value: sourceCompactJson.m
+        }).up();
+    }
+    node.c('ssrc-info', {
+        xmlns: 'http://jitsi.org/jitmeet',
+        owner
+    }).up();
+
+    return node.node;
+}
+
+/**
+ * Creates an "ssrc-group" XML element for the SSRC group described in compact JSON format in [ssrcGroupCompactJson].
+ * @param {*} ssrcGroupCompactJson the compact JSON representation of the SSRC group.
+ * @returns the created "ssrc-group" element.
+ */
+function _createSsrcGroupExtension(ssrcGroupCompactJson) {
+    const node = $build('ssrc-group', {
+        xmlns: 'urn:xmpp:jingle:apps:rtp:ssma:0',
+        semantics: _getSemantics(ssrcGroupCompactJson[0])
+    });
+
+    for (let i = 1; i < ssrcGroupCompactJson.length; i++) {
+        node.c('source', {
+            xmlns: 'urn:xmpp:jingle:apps:rtp:ssma:0',
+            ssrc: ssrcGroupCompactJson[i]
+        }).up();
+    }
+
+    return node.node;
+}
+
+/**
+ * Finds in a Jingle IQ the RTP description element with the given media type. If one does not exists, create it (as
+ *  well as the required  "content" parent element) and adds it to the IQ.
+ * @param {*} iq
+ * @param {*} mediaType The media type, "audio" or "video".
+ * @returns the RTP description element with the given media type.
+ */
+function _getOrCreateRtpDescription(iq, mediaType) {
+    const jingle = $(iq).find('jingle')[0];
+    let content = $(jingle).find(`content[name="${mediaType}"]`);
+    let description;
+
+    if (content.length) {
+        content = content[0];
+    } else {
+        // I'm not suree if "creator" and "senders" are required.
+        content = $build('content', {
+            name: mediaType
+        }).node;
+        jingle.appendChild(content);
+    }
+
+    description = $(content).find('description');
+
+    if (description.length) {
+        description = description[0];
+    } else {
+        description = $build('description', {
+            xmlns: 'urn:xmpp:jingle:apps:rtp:1',
+            media: mediaType
+        }).node;
+        content.appendChild(description);
+    }
+
+    return description;
+}
+
+/**
+ * Converts the short string representing SSRC group semantics in compact JSON format to the standard representation
+ * (i.e. convert "f" to "FID" and "s" to "SIM").
+ * @param {*} str the compact JSON format representation of an SSRC group's semantics.
+ * @returns the SSRC group semantics corresponding to [str].
+ */
+function _getSemantics(str) {
+    if (str === 'f') {
+        return 'FID';
+    } else if (str === 's') {
+        return 'SIM';
+    }
+
+    return null;
+}
+
+/**
+ * Reads a JSON-encoded message (from a "json-message" element) and extracts source descriptions. Adds the extracted
+ * source descriptions to the given Jingle IQ in the standard Jingle format.
+ *
+ * Encoding sources in this compact JSON format instead of standard Jingle was introduced in order to reduce the
+ * network traffic and load on the XMPP server. The format is described in Jicofo [TODO: insert link].
+ *
+ * @param {*} iq the IQ to which source descriptions will be added.
+ * @param {*} jsonMessageXml The XML node for the "json-message" element.
+ * @returns {Map<string, Array<string>} The audio and video ssrcs extracted from the JSON-encoded message with remote
+ * endpoint id as the key.
+ */
+export function expandSourcesFromJson(iq, jsonMessageXml) {
+    let json;
+
+    try {
+        json = JSON.parse(jsonMessageXml.textContent);
+    } catch (error) {
+        logger.error(`json-message XML contained invalid JSON, ignoring: ${jsonMessageXml.textContent}`);
+
+        return null;
+    }
+
+    if (!json?.sources) {
+        // It might be a message of a different type, no need to log.
+        return null;
+    }
+
+    // This is where we'll add "source" and "ssrc-group" elements. Create them elements if they don't exist.
+    const audioRtpDescription = _getOrCreateRtpDescription(iq, MediaType.AUDIO);
+    const videoRtpDescription = _getOrCreateRtpDescription(iq, MediaType.VIDEO);
+    const ssrcMap = new Map();
+
+    for (const owner in json.sources) {
+        if (json.sources.hasOwnProperty(owner)) {
+            const ssrcs = [];
+            const ownerSources = json.sources[owner];
+
+            // The video sources, video ssrc-groups, audio sources and audio ssrc-groups are encoded in that order in
+            // the elements of the array.
+            const videoSources = ownerSources?.length && ownerSources[0];
+            const videoSsrcGroups = ownerSources?.length > 1 && ownerSources[1];
+            const audioSources = ownerSources?.length > 2 && ownerSources[2];
+            const audioSsrcGroups = ownerSources?.length > 3 && ownerSources[3];
+
+            if (videoSources?.length) {
+                for (let i = 0; i < videoSources.length; i++) {
+                    videoRtpDescription.appendChild(_createSourceExtension(owner, videoSources[i]));
+                }
+
+                // Log only the first video ssrc per endpoint.
+                ssrcs.push(videoSources[0]?.s);
+            }
+
+            if (videoSsrcGroups?.length) {
+                for (let i = 0; i < videoSsrcGroups.length; i++) {
+                    videoRtpDescription.appendChild(_createSsrcGroupExtension(videoSsrcGroups[i]));
+                }
+            }
+            if (audioSources?.length) {
+                for (let i = 0; i < audioSources.length; i++) {
+                    audioRtpDescription.appendChild(_createSourceExtension(owner, audioSources[i]));
+                }
+                ssrcs.push(audioSources[0]?.s);
+            }
+
+            if (audioSsrcGroups?.length) {
+                for (let i = 0; i < audioSsrcGroups.length; i++) {
+                    audioRtpDescription.appendChild(_createSsrcGroupExtension(audioSsrcGroups[i]));
+                }
+            }
+            ssrcMap.set(owner, ssrcs);
+        }
+    }
+
+    return ssrcMap;
+}
