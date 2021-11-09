c193f0d4335b6ed4810aa07d0c7d6860b814801a
Hristo Terezov <hristo@jitsi.org>
2021-11-08 18:01:43 -0600

    feat(recording): Add unexpected-request error

--- modules/recording/recordingConstants.js
+++ modules/recording/recordingConstants.js
@@ -3,6 +3,7 @@ export default {
         BUSY: 'busy',
         ERROR: 'error',
         RESOURCE_CONSTRAINT: 'resource-constraint',
+        UNEXPECTED_REQUEST: 'unexpected-request',
         SERVICE_UNAVAILABLE: 'service-unavailable'
     },
     mode: {
