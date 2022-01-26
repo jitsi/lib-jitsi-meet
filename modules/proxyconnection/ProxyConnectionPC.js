jitsi/lib-jitsi-meet

commit 5c9b90511120ae42660176afc0715f8c8886ef68

ref: move SignalingLayer to the conference

modules/proxyconnection/ProxyConnectionPC.js
@@ -4,6 +4,7 @@ import RTCEvents from '../../service/RTC/RTCEvents';
 import XMPPEvents from '../../service/xmpp/XMPPEvents';
 import RTC from '../RTC/RTC';
 import JingleSessionPC from '../xmpp/JingleSessionPC';
+import SignalingLayerImpl from '../xmpp/SignalingLayerImpl';
 import { DEFAULT_STUN_SERVERS } from '../xmpp/xmpp';
 
 import { ACTIONS } from './constants';
@@ -267,6 +268,10 @@ export default class ProxyConnectionPC {
             true, // isP2P
             this._options.isInitiator // isInitiator
         );
+
+        const signalingLayer = new SignalingLayerImpl();
+
+        signalingLayer.setChatRoom(roomStub);
 
         /**
          * An additional initialize call is necessary to properly set instance

JitsiConference.js
@@ -45,6 +45,7 @@ import RandomUtil from './modules/util/RandomUtil';
 import ComponentsVersions from './modules/version/ComponentsVersions';
 import VideoSIPGW from './modules/videosipgw/VideoSIPGW';
 import * as VideoSIPGWConstants from './modules/videosipgw/VideoSIPGWConstants';
+import SignalingLayerImpl from './modules/xmpp/SignalingLayerImpl';
 import {
     FEATURE_E2EE,
     FEATURE_JIGASI,
@@ -141,6 +142,14 @@ export default function JitsiConference(options) {
     this.options = options;
     this.eventManager = new JitsiConferenceEventManager(this);
     this.participants = {};
+
+    /**
+     * The signaling layer instance.
+     * @type {SignalingLayerImpl}
+     * @private
+     */
+    this._signalingLayer = new SignalingLayerImpl();
+
     this._init(options);
     this.componentsVersions = new ComponentsVersions(this);
 
@@ -349,6 +358,8 @@ JitsiConference.prototype._init = function(options = {}) {
         },
         JitsiConference.resourceCreator
     );
+
+    this._signalingLayer.setChatRoom(this.room);
 
     // Connection interrupted/restored listeners
     this._onIceConnectionInterrupted
@@ -653,6 +664,8 @@ JitsiConference.prototype.leave = function() {
         room.removeListener(XMPPEvents.MEETING_ID_SET, this._sendConferenceJoinAnalyticsEvent);
 
         this.eventManager.removeXMPPListeners();
+
+        this._signalingLayer.setChatRoom(null);
 
         this.room = null;
 
@@ -2097,10 +2110,14 @@ JitsiConference.prototype._acceptJvbIncomingCall = function(
         }));
 
     try {
-        jingleSession.initialize(this.room, this.rtc, {
-            ...this.options.config,
-            enableInsertableStreams: this.isE2EEEnabled()
-        });
+        jingleSession.initialize(
+            this.room,
+            this.rtc,
+            this._signalingLayer,
+            {
+                ...this.options.config,
+                enableInsertableStreams: this.isE2EEEnabled()
+            });
     } catch (error) {
         GlobalOnErrorHandler.callErrorHandler(error);
         logger.error(error);
@@ -2862,7 +2879,9 @@ JitsiConference.prototype._acceptP2PIncomingCall = function(
 
     this.p2pJingleSession.initialize(
         this.room,
-        this.rtc, {
+        this.rtc,
+        this._signalingLayer,
+        {
             ...this.options.config,
             enableInsertableStreams: this.isE2EEEnabled()
         });
@@ -3222,7 +3241,9 @@ JitsiConference.prototype._startP2PSession = function(remoteJid) {
 
     this.p2pJingleSession.initialize(
         this.room,
-        this.rtc, {
+        this.rtc,
+        this._signalingLayer,
+        {
             ...this.options.config,
             enableInsertableStreams: this.isE2EEEnabled()
         });

modules/RTC/CodecSelection.js
@@ -131,7 +131,7 @@ export class CodecSelection {
             const remoteParticipants = this.conference.getParticipants().map(participant => participant.getId());
 
             for (const remote of remoteParticipants) {
-                const peerMediaInfo = session.signalingLayer.getPeerMediaInfo(remote, MediaType.VIDEO);
+                const peerMediaInfo = session._signalingLayer.getPeerMediaInfo(remote, MediaType.VIDEO);
                 const peerCodec = peerMediaInfo?.codecType;
 
                 if (peerCodec

modules/xmpp/JingleSession.js
@@ -67,6 +67,13 @@ export default class JingleSession extends Listenable {
         this.room = null;
 
         /**
+         * The signaling layer.
+         * @type {SignalingLayerImpl | null}
+         * @private
+         */
+        this._signalingLayer = null;
+
+        /**
          * Jingle session state - uninitialized until {@link initialize} is
          * called @type {JingleSessionState}
          */
@@ -102,10 +109,11 @@ export default class JingleSession extends Listenable {
      * @param {ChatRoom} room the chat room for the conference associated with
      * this session
      * @param {RTC} rtc the RTC service instance
+     * @param {SignalingLayerImpl} signalingLayer - The signaling layer instance.
      * @param {object} options - the options, see implementing class's
      * {@link #doInitialize} description for more details.
      */
-    initialize(room, rtc, options) {
+    initialize(room, rtc, signalingLayer, options) {
         if (this.state !== null) {
             const errmsg
                 = `attempt to initiate on session ${this.sid}
@@ -114,8 +122,11 @@ export default class JingleSession extends Listenable {
             logger.error(errmsg);
             throw new Error(errmsg);
         }
+
+        // TODO decouple from room
         this.room = room;
         this.rtc = rtc;
+        this._signalingLayer = signalingLayer;
         this.state = JingleSessionState.PENDING;
         this.doInitialize(options);
     }

modules/xmpp/JingleSessionPC.js
@@ -23,7 +23,6 @@ import browser from './../browser';
 import JingleSession from './JingleSession';
 import * as JingleSessionState from './JingleSessionState';
 import MediaSessionEvents from './MediaSessionEvents';
-import SignalingLayerImpl from './SignalingLayerImpl';
 import XmppConnection from './XmppConnection';
 
 const logger = getLogger(__filename);
@@ -263,12 +262,6 @@ export default class JingleSessionPC extends JingleSession {
         this.remoteRecvMaxFrameHeight = undefined;
 
         /**
-         * The signaling layer implementation.
-         * @type {SignalingLayerImpl}
-         */
-        this.signalingLayer = new SignalingLayerImpl();
-
-        /**
          * The queue used to serialize operations done on the peerconnection.
          *
          * @type {AsyncQueue}
@@ -387,7 +380,7 @@ export default class JingleSessionPC extends JingleSession {
 
         this.peerconnection
             = this.rtc.createPeerConnection(
-                    this.signalingLayer,
+                    this._signalingLayer,
                     this.pcConfig,
                     this.isP2P,
                     pcOptions);
@@ -630,9 +623,6 @@ export default class JingleSessionPC extends JingleSession {
                     });
             }
         };
-
-        // The signaling layer will bind it's listeners at this point
-        this.signalingLayer.setChatRoom(this.room);
     }
 
     /**
@@ -886,7 +876,7 @@ export default class JingleSessionPC extends JingleSession {
 
             if (this.isP2P) {
                 // In P2P all SSRCs are owner by the remote peer
-                this.signalingLayer.setSSRCOwner(
+                this._signalingLayer.setSSRCOwner(
                     ssrc, Strophe.getResourceFromJid(this.remoteJid));
             } else {
                 $(ssrcElement)
@@ -898,7 +888,7 @@ export default class JingleSessionPC extends JingleSession {
                             if (isNaN(ssrc) || ssrc < 0) {
                                 logger.warn(`${this} Invalid SSRC ${ssrc} value received for ${owner}`);
                             } else {
-                                this.signalingLayer.setSSRCOwner(
+                                this._signalingLayer.setSSRCOwner(
                                     ssrc,
                                     getEndpointId(owner));
                             }
@@ -2637,9 +2627,6 @@ export default class JingleSessionPC extends JingleSession {
 
         logger.debug(`${this} Queued PC close task`);
         this.modificationQueue.push(finishCallback => {
-            // The signaling layer will remove it's listeners
-            this.signalingLayer.setChatRoom(null);
-
             // do not try to close if already closed.
             this.peerconnection && this.peerconnection.close();
             finishCallback();

modules/xmpp/SignalingLayerImpl.js
@@ -104,6 +104,12 @@ export default class SignalingLayerImpl extends SignalingLayer {
         if (typeof ssrc !== 'number') {
             throw new TypeError(`SSRC(${ssrc}) must be a number`);
         }
+
+        // Now signaling layer instance is shared between different JingleSessionPC instances, so although very unlikely
+        // an SSRC conflict could potentially occur. Log a message to make debugging easier.
+        if (this.ssrcOwners.has(ssrc)) {
+            logger.error(`SSRC owner re-assigned from ${this.ssrcOwners.get(ssrc)} to ${endpointId}`);
+        }
         this.ssrcOwners.set(ssrc, endpointId);
     }
 }

commit 393274a104fdc534a92f7ab70ee2abec72c047a0

fix(ProxyConnection) set signaling layer
Regression introduced in
https://github.com/jitsi/lib-jitsi-meet/commit/5c9b90511120ae42660176afc0715f8c8886ef68

modules/proxyconnection/ProxyConnectionPC.js
@@ -277,7 +277,7 @@ export default class ProxyConnectionPC {
          * An additional initialize call is necessary to properly set instance
          * variable for calling.
          */
-        peerConnection.initialize(roomStub, this._rtc, configStub);
+        peerConnection.initialize(roomStub, this._rtc, signalingLayer, configStub);
 
         return peerConnection;
     }