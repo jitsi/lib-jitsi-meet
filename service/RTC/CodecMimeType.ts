a2340787f8ced7c84f1dc6fc971660c52380303a
Gary Hunt <gary.hunt@datadeliverynetwork.com>
2022-02-09 20:10:24 +0000

    TypeScript enum for CodecMimeType

--- service/RTC/CodecMimeType.ts
+++ service/RTC/CodecMimeType.ts
@@ -2,32 +2,32 @@
  * Enumeration of the codec mime types
  * @type {{H264: string, OPUS: string, ULPFEC: string, VP8: string, VP9: string}}
  */
-const CodecMimeType = {
+enum CodecMimeType {
     /**
      * The h264 codec mime type.
      */
-    H264: 'h264',
+    H264 = 'h264',
 
     /**
      * The opus codec mime type.
      */
-    OPUS: 'opus',
+    OPUS = 'opus',
 
     /**
      * The ulpfec codec mime type.
      */
-    ULPFEC: 'ulpfec',
+    ULPFEC = 'ulpfec',
 
     /**
      * The vp8 codec mime type.
      */
-    VP8: 'vp8',
+    VP8 = 'vp8',
 
     /**
      * The vp9 codec mime type.
      */
-    VP9: 'vp9'
+    VP9 = 'vp9'
 
 };
 
-module.exports = CodecMimeType;
+export default CodecMimeType;
