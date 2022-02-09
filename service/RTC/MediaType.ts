951556d52e5655def680012d770621bab50af9cd
Gary Hunt <gary.hunt@datadeliverynetwork.com>
2022-02-09 20:02:18 +0000

    TypeScript enum for MediaType

---
+++ service/RTC/MediaType.ts
@@ -0,0 +1,21 @@
+export enum MediaTypes {
+    /**
+     * The audio type.
+     */
+    AUDIO = 'audio',
+
+    /**
+     * The presenter type.
+     */
+    PRESENTER = 'presenter',
+
+    /**
+     * The video type.
+     */
+    VIDEO = 'video',
+};
+
+// exported for backward compatibility
+export const AUDIO = MediaTypes.AUDIO;
+export const PRESENTER = MediaTypes.PRESENTER;
+export const VIDEO = MediaTypes.VIDEO;
