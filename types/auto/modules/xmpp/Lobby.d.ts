554b95e9d6b8616a91e62eaa1ac56397646058f2
Дамян Минков <damencho@jitsi.org>
2022-02-10 08:22:13 -0600

    fix: Adds missing ts files.

--- types/auto/modules/xmpp/Lobby.d.ts
+++ types/auto/modules/xmpp/Lobby.d.ts
@@ -31,6 +31,43 @@ export default class Lobby {
      */
     disable(): void;
     /**
+     * Broadcast a message to all participants in the lobby room
+     * @param {Object} message The message to send
+     *
+     * @returns {void}
+     */
+    sendMessage(message: any): void;
+    /**
+     * Sends a private message to a participant in a lobby room.
+     * @param {string} id The message to send
+     * @param {Object} message The message to send
+     *
+     * @returns {void}
+     */
+    sendPrivateMessage(id: string, message: any): void;
+    /**
+     * Gets the local id for a participant in a lobby room.
+     * This is used for lobby room private chat messages.
+     *
+     * @returns {string}
+     */
+    getLocalId(): string;
+    /**
+     * Adds a message listener to the lobby room.
+     * @param {Function} listener The listener function,
+     * called when a new message is received in the lobby room.
+     *
+     * @returns {Function} Handler returned to be able to remove it later.
+     */
+    addMessageListener(listener: Function): Function;
+    /**
+     * Remove a message handler from the lobby room.
+     * @param {Function} handler The handler function to remove.
+     *
+     * @returns {void}
+     */
+    removeMessageHandler(handler: Function): void;
+    /**
      * Leaves the lobby room.
      *
      * @returns {Promise}
