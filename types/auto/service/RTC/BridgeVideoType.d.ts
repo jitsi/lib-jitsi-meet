a89a0cc037943c400841f15cde52d3670c6bfca1
Gary Hunt <gary.hunt@datadeliverynetwork.com>
2022-02-21 16:27:58 +0000

    updated based upon use of modules.exports = syntax

--- types/auto/service/RTC/BridgeVideoType.d.ts
+++ types/auto/service/RTC/BridgeVideoType.d.ts
@@ -1,11 +1,27 @@
-export = BridgeVideoType;
 /**
  * Enumeration of the video types that are signaled to the bridge
  * @type {{CAMERA: string, DESKTOP: string, DESKTOP_HIGH_FPS: string, NONE: string}}
  */
-declare const BridgeVideoType: {
-    CAMERA: string;
-    DESKTOP: string;
-    DESKTOP_HIGH_FPS: string;
-    NONE: string;
-};
+export declare enum BridgeVideoType {
+    /**
+     * The camera video type.
+     */
+    CAMERA = "camera",
+    /**
+     * The low fps desktop video type.
+     */
+    DESKTOP = "desktop",
+    /**
+     * The high fps desktop video type.
+     */
+    DESKTOP_HIGH_FPS = "desktop_high_fps",
+    /**
+     * Video type when no local source is present.
+     */
+    NONE = "none"
+}
+export declare const CAMERA = BridgeVideoType.CAMERA;
+export declare const DESKTOP = BridgeVideoType.DESKTOP;
+export declare const DESKTOP_HIGH_FPS = BridgeVideoType.DESKTOP_HIGH_FPS;
+export declare const NONE = BridgeVideoType.NONE;
+export default BridgeVideoType;
