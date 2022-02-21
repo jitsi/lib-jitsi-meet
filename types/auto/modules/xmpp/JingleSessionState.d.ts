36ccd22202e84170443b2f53cd7fadc3c3812031
Gary Hunt <gary.hunt@datadeliverynetwork.com>
2022-02-21 14:15:37 +0000

    include .d.ts

--- types/auto/modules/xmpp/JingleSessionState.d.ts
+++ types/auto/modules/xmpp/JingleSessionState.d.ts
@@ -1,20 +1,20 @@
-/**
- * The pending Jingle session state which means the session as defined in
- * XEP-0166(before 'session-invite/session-accept' took place).
- *
- * @type {string}
- */
-export const PENDING: string;
-/**
- * The active Jingle session state as defined in XEP-0166
- * (after 'session-invite'/'session-accept').
- *
- * @type {string}
- */
-export const ACTIVE: string;
-/**
- * The ended Jingle session state as defined in XEP-0166
- * (after 'session-terminate').
- * @type {string}
- */
-export const ENDED: string;
+export declare enum JingleSessionState {
+    /**
+     * The pending Jingle session state which means the session as defined in
+     * XEP-0166(before 'session-invite/session-accept' took place).
+     */
+    PENDING = "pending",
+    /**
+     * The active Jingle session state as defined in XEP-0166
+     * (after 'session-invite'/'session-accept').
+     */
+    ACTIVE = "active",
+    /**
+     * The ended Jingle session state as defined in XEP-0166
+     * (after 'session-terminate').
+     */
+    ENDED = "ended"
+}
+export declare const PENDING = JingleSessionState.PENDING;
+export declare const ACTIVE = JingleSessionState.ACTIVE;
+export declare const ENDED = JingleSessionState.ENDED;
