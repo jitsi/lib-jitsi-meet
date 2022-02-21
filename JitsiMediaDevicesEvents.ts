581ee807f7a89e1800331ec999e3f9494a56bd6d
Gary Hunt <garyhuntddn@users.noreply.github.com>
2022-02-21 13:30:04 +0000

    feat(ts) TypeScript enum for JitsiMediaDevicesEvents

--- JitsiMediaDevicesEvents.ts
+++ JitsiMediaDevicesEvents.ts
@@ -1,7 +1,8 @@
 /**
  * The events for the media devices.
  */
-export declare enum JitsiMediaDevicesEvents {
+
+export enum JitsiMediaDevicesEvents {
     /**
      * Indicates that the list of available media devices has been changed. The
      * event provides the following parameters to its listeners:
@@ -10,13 +11,15 @@ export declare enum JitsiMediaDevicesEvents {
      *  MediaDeviceInfo-like objects that are currently connected.
      *  @see https://developer.mozilla.org/en-US/docs/Web/API/MediaDeviceInfo
      */
-    DEVICE_LIST_CHANGED = "mediaDevices.devicechange",
+    DEVICE_LIST_CHANGED = 'mediaDevices.devicechange',
+
     /**
      * Event emitted when the user granted/blocked a permission for the camera / mic.
      * Used to keep track of the granted permissions on browsers which don't
      * support the Permissions API.
      */
-    PERMISSIONS_CHANGED = "rtc.permissions_changed",
+    PERMISSIONS_CHANGED = 'rtc.permissions_changed',
+
     /**
      * Indicates that the environment is currently showing permission prompt to
      * access camera and/or microphone. The event provides the following
@@ -26,10 +29,13 @@ export declare enum JitsiMediaDevicesEvents {
      *  |'react-native'|'android'} environmentType - type of browser or
      *  other execution environment.
      */
-    PERMISSION_PROMPT_IS_SHOWN = "mediaDevices.permissionPromptIsShown",
-    SLOW_GET_USER_MEDIA = "mediaDevices.slowGetUserMedia"
-}
-export declare const DEVICE_LIST_CHANGED = JitsiMediaDevicesEvents.DEVICE_LIST_CHANGED;
-export declare const PERMISSIONS_CHANGED = JitsiMediaDevicesEvents.PERMISSIONS_CHANGED;
-export declare const PERMISSION_PROMPT_IS_SHOWN = JitsiMediaDevicesEvents.PERMISSION_PROMPT_IS_SHOWN;
-export declare const SLOW_GET_USER_MEDIA = JitsiMediaDevicesEvents.SLOW_GET_USER_MEDIA;
+    PERMISSION_PROMPT_IS_SHOWN = 'mediaDevices.permissionPromptIsShown',
+
+    SLOW_GET_USER_MEDIA = 'mediaDevices.slowGetUserMedia'
+};
+
+// exported for backward compatibility
+export const DEVICE_LIST_CHANGED = JitsiMediaDevicesEvents.DEVICE_LIST_CHANGED;
+export const PERMISSIONS_CHANGED = JitsiMediaDevicesEvents.PERMISSIONS_CHANGED;
+export const PERMISSION_PROMPT_IS_SHOWN = JitsiMediaDevicesEvents.PERMISSION_PROMPT_IS_SHOWN;
+export const SLOW_GET_USER_MEDIA = JitsiMediaDevicesEvents.SLOW_GET_USER_MEDIA;
