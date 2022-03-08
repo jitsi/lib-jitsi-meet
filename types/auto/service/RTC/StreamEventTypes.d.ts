b59db5d57567999bb5e5f3f5bf74a88e881db38f
Gary Hunt <garyhuntddn@users.noreply.github.com>
2022-03-08 17:20:13 +0000

    chore(cleanup) remove unused code

--- types/auto/service/RTC/StreamEventTypes.d.ts
+++
@@ -1,6 +0,0 @@
-export const EVENT_TYPE_LOCAL_CREATED: string;
-export const EVENT_TYPE_LOCAL_CHANGED: string;
-export const EVENT_TYPE_LOCAL_ENDED: string;
-export const EVENT_TYPE_REMOTE_CREATED: string;
-export const EVENT_TYPE_REMOTE_ENDED: string;
-export const TRACK_MUTE_CHANGED: string;
