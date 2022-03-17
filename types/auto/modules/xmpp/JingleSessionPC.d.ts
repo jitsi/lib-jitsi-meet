f8a92930ccf58f748ff8f44cc0a30a08b339e315
Jaya Allamsetty <54324652+jallamsetty1@users.noreply.github.com>
2022-03-17 15:40:00 -0400

    fix(multi-stream) Add video tracks sequencially. If there are more than one video track at pc creation time, add them sequencially to avoid renegotiation loop.

--- types/auto/modules/xmpp/JingleSessionPC.d.ts
+++ types/auto/modules/xmpp/JingleSessionPC.d.ts
@@ -459,11 +459,11 @@ export default class JingleSessionPC extends JingleSession {
      * Adds a new track to the peerconnection. This method needs to be called only when a secondary JitsiLocalTrack is
      * being added to the peerconnection for the first time.
      *
-     * @param {JitsiLocalTrack} localTrack track to be added to the peer connection.
+     * @param {Array<JitsiLocalTrack>} localTracks - Tracks to be added to the peer connection.
      * @returns {Promise<void>} that resolves when the track is successfully added to the peerconnection, rejected
      * otherwise.
      */
-    addTrack(localTrack: any): Promise<void>;
+    addTracks(localTracks?: Array<any>): Promise<void>;
     /**
      * Replaces <tt>oldTrack</tt> with <tt>newTrack</tt> and performs a single
      * offer/answer cycle after both operations are done. Either
