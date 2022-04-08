fa916d41ed79a3d145ffeffef74ed14577a85f4f
Jaya Allamsetty <54324652+jallamsetty1@users.noreply.github.com>
2022-04-08 12:50:14 -0400

    fix(BridgeChannel): re-send constraints and videoType message after every re-connect. This fixes an issue where the receiver constraints and video type messages are sent on the bridge channel only the first time the ws conn gets re-established.

--- modules/RTC/RTC.js
+++ modules/RTC/RTC.js
@@ -195,10 +195,7 @@ export default class RTC extends Listenable {
         RTCUtils.removeListener(RTCEvents.DEVICE_LIST_CHANGED, this._onDeviceListChanged);
 
         if (this._channelOpenListener) {
-            this.removeListener(
-                RTCEvents.DATA_CHANNEL_OPEN,
-                this._channelOpenListener
-            );
+            this.removeListener(RTCEvents.DATA_CHANNEL_OPEN, this._channelOpenListener);
         }
     }
 
@@ -281,9 +278,6 @@ export default class RTC extends Listenable {
                     logError(error, 'VideoTypeMessage', this._videoType);
                 }
             }
-
-            this.removeListener(RTCEvents.DATA_CHANNEL_OPEN, this._channelOpenListener);
-            this._channelOpenListener = null;
         };
         this.addListener(RTCEvents.DATA_CHANNEL_OPEN, this._channelOpenListener);
 
