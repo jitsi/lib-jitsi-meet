417781db6cce61c24a70d8e196eb8e72aee9c11c
Jorge Oliveira <neweracracker@gmail.com>
2022-04-22 12:39:15 +0100

    chore(deps): bump async to 3.2.3

--- types/auto/modules/util/AsyncQueue.d.ts
+++ types/auto/modules/util/AsyncQueue.d.ts
@@ -2,7 +2,7 @@
  * A queue for async task execution.
  */
 export default class AsyncQueue {
-    _queue: any;
+    _queue: import("async").QueueObject<any>;
     _stopped: boolean;
     /**
      * Removes any pending tasks from the queue.
