c6d05e5b6cf39cb3e4352ce91cc0ee7f79b8b0cf
bgrozev <boris@jitsi.org>
2022-07-06 12:32:35 -0500

    Remove broken isScreenSharing function. (#2057)

--- modules/RTC/JitsiTrack.js
+++ modules/RTC/JitsiTrack.js
@@ -388,14 +388,6 @@ export default class JitsiTrack extends EventEmitter {
     }
 
     /**
-     * Returns true if this is a video track and the source of the video is a
-     * screen capture as opposed to a camera.
-     */
-    isScreenSharing() {
-        // FIXME: Should be fixed or removed.
-    }
-
-    /**
      * Returns id of the track.
      * @returns {string|null} id of the track or null if this is fake track.
      */
