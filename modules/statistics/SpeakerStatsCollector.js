ec36be77dae183c7f0d543331dbcf08a0d238feb
alexander.soloviov <alexander.soloviov@wildix.com>
2022-08-25 11:51:42 +0100

    Speaker stats collector throws errors when joining a conference which Jibri is already recording

--- modules/statistics/SpeakerStatsCollector.js
+++ modules/statistics/SpeakerStatsCollector.js
@@ -172,12 +172,12 @@ export default class SpeakerStatsCollector {
                     this.stats.users[userId] = speakerStatsToUpdate;
                     speakerStatsToUpdate.markAsHasLeft();
                 }
+
+                speakerStatsToUpdate.totalDominantSpeakerTime
+                    = newStats[userId].totalDominantSpeakerTime;
+
+                speakerStatsToUpdate.setFaceExpressions(newStats[userId].faceExpressions);
             }
-
-            speakerStatsToUpdate.totalDominantSpeakerTime
-                = newStats[userId].totalDominantSpeakerTime;
-
-            speakerStatsToUpdate.setFaceExpressions(newStats[userId].faceExpressions);
         }
     }
 }
