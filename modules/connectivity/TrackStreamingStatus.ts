0748d89a991de6cecb74e679c1cb4afff5e59c80
William Liang <WilliamLiang918@gmail.com>
2023-06-02 13:03:08 -0400

    fix: Remove TRACK_VIDEOTYPE_CHANGED listener on dispose

--- modules/connectivity/TrackStreamingStatus.ts
+++ modules/connectivity/TrackStreamingStatus.ts
@@ -302,6 +302,7 @@ export class TrackStreamingStatusImpl {
             this.rtc.removeListener(RTCEvents.REMOTE_TRACK_UNMUTE, this._onTrackRtcUnmuted);
 
             this.track.off(JitsiTrackEvents.TRACK_MUTE_CHANGED, this._onSignallingMuteChanged);
+            this.track.off(JitsiTrackEvents.TRACK_VIDEOTYPE_CHANGED, this._onTrackVideoTypeChanged);
         }
 
         this.conference.off(JitsiConferenceEvents.FORWARDED_SOURCES_CHANGED, this._onForwardedSourcesChanged);
