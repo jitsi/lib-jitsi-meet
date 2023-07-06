23253902261cf812d9dcfe860774ac352992f3c7
damencho <damencho@jitsi.org>
2023-07-05 23:42:21 -0500

    fix: Fixes authentication and renamed method.
    Fixes jitsi/jitsi-meet#13455.

--- authenticateAndUpgradeRole.js
+++ authenticateAndUpgradeRole.js
@@ -109,9 +109,7 @@ export default function authenticateAndUpgradeRole({
 
                         // At this point we should have the new session ID
                         // stored in the settings. Send a new conference IQ.
-                        this.room.moderator.allocateConferenceFocus();
-
-                        resolve();
+                        this.room.moderator.sendConferenceRequest().finally(resolve);
                     })
                     .catch(({ error, message }) => {
                         xmpp.disconnect();
