b794804ca9da02e20890bd41e12e4fa6c58c31cd
Daniel McAssey <daniel.mcassey@gmail.com>
2023-12-06 12:15:50 +0000

    fix(Listenable): use correct implementation of Listenable extending EventEmitter instead of creating

--- modules/util/Listenable.js
+++ modules/util/Listenable.js
@@ -1,43 +1,20 @@
-import EventEmitter from 'events';
+import { EventEmitter } from 'events';
 
 /**
  * The class implements basic event operations - add/remove listener.
  * NOTE: The purpose of the class is to be extended in order to add
  * this functionality to other classes.
  */
-export default class Listenable {
+export default class Listenable extends EventEmitter {
     /**
      * Creates new instance.
-     * @param {EventEmitter} eventEmitter
      * @constructor
      */
-    constructor(eventEmitter = new EventEmitter()) {
-        this.eventEmitter = eventEmitter;
+    constructor() {
+        super();
 
         // aliases for addListener/removeListener
         this.addEventListener = this.on = this.addListener;
         this.removeEventListener = this.off = this.removeListener;
-    }
-
-    /**
-     * Adds new listener.
-     * @param {String} eventName the name of the event
-     * @param {Function} listener the listener.
-     * @returns {Function} - The unsubscribe function.
-     */
-    addListener(eventName, listener) {
-        this.eventEmitter.addListener(eventName, listener);
-
-        return () => this.removeEventListener(eventName, listener);
-    }
-
-    /**
-     * Removes listener.
-     * @param {String} eventName the name of the event that triggers the
-     * listener
-     * @param {Function} listener the listener.
-     */
-    removeListener(eventName, listener) {
-        this.eventEmitter.removeListener(eventName, listener);
     }
 }
