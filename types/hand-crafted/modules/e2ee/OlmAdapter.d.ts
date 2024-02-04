74707c06175009774a54d65e21e842fa1889dc2d
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-02-04 11:19:19 +0100

    feat:update Olmadapter types

--- types/hand-crafted/modules/e2ee/OlmAdapter.d.ts
+++ types/hand-crafted/modules/e2ee/OlmAdapter.d.ts
@@ -3,9 +3,9 @@ import Listenable from '../util/Listenable';
 
 export class OlmAdapter extends Listenable {
   constructor( conference: JitsiConference );
-  initSessions: () => Promise<unknown>; // TODO:
+  initSessionsAndSetMediaKey: ( key: Uint8Array, pqkey: Uint8Array ) => Promise<Uint8Array>; // TODO:
   static isSupported: () => boolean;
-  updateCurrentMediaKey: ( key: Uint8Array, pqkey: Uint8Array | boolean ) => number;
+  updateCurrentMediaKey: ( key: Uint8Array, pqkey: Uint8Array ) => Uint8Array;
   clearParticipantSession: ( participant: unknown ) => void; // TODO:
   clearAllParticipantsSessions: () => void;
   updateKey: ( key: Uint8Array , pqkey: Uint8Array | boolean ) => Promise<number>;
