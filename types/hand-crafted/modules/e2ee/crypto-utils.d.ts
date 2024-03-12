026ae6f63eec536a3901586786eaf871e7e70022
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-03-12 12:12:19 +0100

    chore: update types

--- types/hand-crafted/modules/e2ee/crypto-utils.d.ts
+++ types/hand-crafted/modules/e2ee/crypto-utils.d.ts
@@ -1,5 +1,8 @@
-export function deriveKeys( material: CryptoKey ): Promise<unknown>; // TODO:
+export function deriveKeys( material: CryptoKey ): Promise<{
+    material: CryptoKey;
+    encryptionKey: CryptoKey;
+}> ; // TODO:
 
-export function ratchet( material: CryptoKey ): Promise<ArrayBuffer>; // TODO: check promise
+export function ratchet( material: CryptoKey ): Promise<Uint8Array>; // TODO: check promise
 
-export function importKey( keyBytes: ArrayBuffer, keyUsages: Array<unknown> ): Promise<CryptoKey>; // TODO: check promise
+export function importKey( keyBytes: Uint8Array): Promise<CryptoKey>; // TODO: check promise
