eb058431f4b4aa58e5e598d1cc58a6dcfdfcc61d
Rafa <rafijv@gmail.com>
2024-03-22 16:49:35 +0100

    feat: updating types

--- types/hand-crafted/modules/xmpp/JingleSessionPC.d.ts
+++ types/hand-crafted/modules/xmpp/JingleSessionPC.d.ts
@@ -4,10 +4,11 @@ import JitsiLocalTrack from '../RTC/JitsiLocalTrack';
 import { CodecMimeType } from '../../service/RTC/CodecMimeType';
 import JitsiRemoteTrack from '../RTC/JitsiRemoteTrack';
 
+
 export default class JingleSessionPC extends JingleSession {
   static parseVideoSenders: ( jingleContents: JQuery ) => string | null;
   constructor( sid: string, localJid: string, remoteJid: string, connection: XmppConnection, mediaConstraints: unknown, iceConfig: unknown, isP2P: boolean, isInitiator: boolean ); // TODO:
-  doInitialize: ( options: {} ) => void;
+  doInitialize: ( options: unknown ) => void;
   getRemoteRecvMaxFrameHeight: () => number | undefined;
   sendIceCandidate: ( candidate: RTCIceCandidate ) => void;
   sendIceCandidates: ( candidates: RTCIceCandidate[] ) => void;
