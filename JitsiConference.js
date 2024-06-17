eab6191e92227b1ea82c2543a206d577bcbbbe75
Rafa <rafijv@gmail.com>
2024-06-17 16:49:35 +0200

    WIP (fisxing small issues due to the master merges)

--- JitsiConference.js
+++ JitsiConference.js
@@ -80,7 +80,7 @@ const logger = getLogger(__filename);
  * {@link ACTION_JINGLE_SI_TIMEOUT} analytics event is sent (in ms).
  * @type {number}
  */
-const JINGLE_SI_TIMEOUT = 5 * 1000;
+const JINGLE_SI_TIMEOUT = 5000;
 
 /**
  * Checks if a given string is a valid video codec mime type.
