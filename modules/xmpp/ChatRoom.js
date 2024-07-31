0566109c0277177393fd87b289b110ce538218d4
Patrick He <77468352+he-patrick@users.noreply.github.com>
2024-07-31 08:38:38 -0400

    Fix import order

--- modules/xmpp/ChatRoom.js
+++ modules/xmpp/ChatRoom.js
@@ -1,10 +1,9 @@
-import { v4 as uuidv4 } from 'uuid';
-
 import { safeJsonParse } from '@jitsi/js-utils/json';
 import { getLogger } from '@jitsi/logger';
 import $ from 'jquery';
 import isEqual from 'lodash.isequal';
 import { $iq, $msg, $pres, Strophe } from 'strophe.js';
+import { v4 as uuidv4 } from 'uuid';
 
 import { AUTH_ERROR_TYPES } from '../../JitsiConferenceErrors';
 import * as JitsiTranscriptionStatus from '../../JitsiTranscriptionStatus';
