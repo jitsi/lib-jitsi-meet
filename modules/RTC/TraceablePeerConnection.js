c8431e640660f1d621dbd89a242616e801ff763e
Saúl Ibarra Corretgé <s@saghul.net>
2024-08-23 15:27:31 +0200

    fix(TPC) fix getting video SSRCs if local one is not present yet

--- modules/RTC/TraceablePeerConnection.js
+++ modules/RTC/TraceablePeerConnection.js
@@ -826,7 +826,7 @@ TraceablePeerConnection.prototype.getTrackBySSRC = function(ssrc) {
         throw new Error(`SSRC ${ssrc} is not a number`);
     }
     for (const localTrack of this.localTracks.values()) {
-        const { ssrcs } = this.localSSRCs.get(localTrack.rtcId);
+        const { ssrcs } = this.localSSRCs.get(localTrack.rtcId) ?? { ssrcs: [] };
 
         if (ssrcs.find(localSsrc => Number(localSsrc) === ssrc)) {
             return localTrack;
