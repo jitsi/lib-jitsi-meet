9eefac111df2758782e0beb25586f6479818efbd
Jaya Allamsetty <jaya.allamsetty@8x8.com>
2024-10-30 16:38:45 -0400

    fix(JingleSession) Get correct direction for source-remove on p2p.

--- modules/xmpp/JingleSessionPC.js
+++ modules/xmpp/JingleSessionPC.js
@@ -400,9 +400,11 @@ export default class JingleSessionPC extends JingleSession {
             for (const mid of modifiedMids) {
                 if (this.isP2P) {
                     const { media } = SDPUtil.parseMLine(currentRemoteSdp.media[mid].split('\r\n')[0]);
-                    const desiredDirection = this.peerconnection.getDesiredMediaDirection(media, true);
+                    const desiredDirection = this.peerconnection.getDesiredMediaDirection(media, isAdd);
+                    const currentDirections = isAdd ? [ MediaDirection.RECVONLY, MediaDirection.INACTIVE ]
+                        : [ MediaDirection.SENDRECV, MediaDirection.SENDONLY ];
 
-                    [ MediaDirection.RECVONLY, MediaDirection.INACTIVE ].forEach(direction => {
+                    currentDirections.forEach(direction => {
                         currentRemoteSdp.media[mid] = currentRemoteSdp.media[mid]
                             .replace(`a=${direction}`, `a=${desiredDirection}`);
                     });
