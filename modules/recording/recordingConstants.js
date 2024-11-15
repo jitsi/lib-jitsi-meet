9652999d389db21eefc9b99822f890e9a585403d
damencho <damencho@jitsi.org>
2024-11-15 12:36:11 -0600

    feat(recording): Adds one more error type in constants.

--- modules/recording/recordingConstants.js
+++ modules/recording/recordingConstants.js
@@ -2,6 +2,7 @@ export default {
     error: {
         BUSY: 'busy',
         ERROR: 'error',
+        POLICY_VIOLATION: 'policy-violation',
         RESOURCE_CONSTRAINT: 'resource-constraint',
         UNEXPECTED_REQUEST: 'unexpected-request',
         SERVICE_UNAVAILABLE: 'service-unavailable'
