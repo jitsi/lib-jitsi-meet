e1aed73f986edcbf48719eefdaaa9ab8c5dfaf55
naman9271 <namanjain9271@gmail.com>
2025-02-14 20:10:58 +0530

    done-bridgechannel.d.ts

--- types/hand-crafted/modules/RTC/BridgeChannel.d.ts
+++ types/hand-crafted/modules/RTC/BridgeChannel.d.ts
@@ -2,6 +2,11 @@ import { ReceiverVideoConstraints } from "../qualitycontrol/ReceiveVideoControll
 import { EventEmitter } from "events";
 import JitsiConference from '../../JitsiConference';
 
+// Defining these types as don't defined in modules/RTC/BridgeChannel.js
+type SourceName = string;
+type BridgeVideoType = "camera" | "desktop" | "presentation" | "other" | string;
+
+
 export default class BridgeChannel {
   constructor(
     peerconnection: RTCPeerConnection | null, 
@@ -9,12 +14,25 @@ export default class BridgeChannel {
     emitter: EventEmitter, 
     conference: JitsiConference
   );
-  
+  _initWebSocket:() => void;
+  _startConnectionRetries : ()=> void;
+  reload:()=>void;
+  _stopConnectionRetries :()=>void;
+  _retryWebSocketConnection: (closeEvent:CloseEvent) =>void;
   mode: () => null | "datachannel" | "websocket";
   close: () => void;
   isOpen: () => boolean;
+  sendEndpointsMessage: (payload: Record<string, unknown>)=>void;
   sendMessage: (to: string, payload: Record<string, unknown>) => void;
   sendSetLastNMessage: (value: number) => void;
   sendEndpointStatsMessage: (payload: Record<string, unknown>) => void;
   sendReceiverVideoConstraintsMessage: (constraints: ReceiverVideoConstraints) => void;
+
+  sendSourceVideoTypeMessage:(sourceName:SourceName, videoType: BridgeVideoType)=>void; 
+  _handleChannel(channel: RTCDataChannel | WebSocket): void;
+  onopen: (() => void) | null;
+  onerror: ((event: Event) => void) | null;
+  onmessage: ((event: MessageEvent) => void) | null;
+  onclose: ((event: CloseEvent) => void) | null;
+  _send:(jsonObject:Record<string,unknown>) => void;
 }
