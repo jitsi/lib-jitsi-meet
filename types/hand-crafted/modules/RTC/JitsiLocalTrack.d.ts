fd1a1116647ba994bb8c51c762a5a7445a593ac5
naman9271 <namanjain9271@gmail.com>
2025-02-16 22:55:53 +0530

    done JistiLocalTrack.d.ts

--- types/hand-crafted/modules/RTC/JitsiLocalTrack.d.ts
+++ types/hand-crafted/modules/RTC/JitsiLocalTrack.d.ts
@@ -2,18 +2,53 @@ import JitsiTrack from './JitsiTrack';
 import { CameraFacingMode } from '../../service/RTC/CameraFacingMode';
 
 export default class JitsiLocalTrack extends JitsiTrack {
-  constructor( trackInfo: { rtcId: number, stream: unknown, track: unknown, mediaType: unknown, videoType: unknown, effects: unknown, resolution: unknown, deviceId: string, facingMode: CameraFacingMode, sourceId: unknown } ) // TODO:
-  isEnded: () => boolean;
-  setEffect: ( effect: unknown ) => Promise<unknown>; // TODO:
-  mute: () => Promise<void>;
-  unmute: () => Promise<void>;
-  dispose: () => Promise<void>;
-  isMuted: () => boolean;
-  isLocal: () => true;
-  getDeviceId: () => string;
-  getParticipantId: () => string;
-  getCameraFacingMode: () => CameraFacingMode | undefined;
-  stopStream: () => void;
-  isReceivingData: () => boolean;
-  toString: () => string;
+  constructor(trackInfo: { 
+    rtcId: number;
+    stream: MediaStream; 
+    track: MediaStreamTrack; 
+    mediaType: string; 
+    videoType: string; 
+    effects: Array<object>; 
+    resolution?: any;  // No information provided about 'resolution', so kept it as any.
+    deviceId: string; 
+    facingMode: CameraFacingMode; 
+    sourceId?: string; 
+    sourceType?: string;
+    constraints: Object;
+})
+_addStreamToConferenceAsUnmute: () => Promise<void>;
+_fireNoDataFromSourceEvent: () => void;
+_initNoDataFromSourceHandlers: () => void;
+_isNoDataFromSourceEventsEnabled: () => boolean;
+_queueSetMuted: (muted: boolean) => Promise<void>;
+_removeStreamFromConferenceAsMute: (successCallback: () => void, errorCallback: (error: Error) => void) => void;
+_sendMuteStatus: (mute: boolean) => void;
+_setMuted: (muted: boolean) => Promise<void>;
+_setRealDeviceIdFromDeviceList: (devices: MediaDeviceInfo[]) => void;
+_setStream: (stream: MediaStream | null) => void;
+_startStreamEffect: (effect: object) => void;
+_stopStreamEffect: () => void;
+_switchCamera: () => void;
+_switchStreamEffect: (effect?: object) => void;
+dispose: () => Promise<void>;
+getCameraFacingMode: () => CameraFacingMode | undefined;
+getCaptureResolution: () => number;
+getDeviceId: () => string;
+getDuration: () => number;
+getParticipantId: () => string;
+getSourceName: () => string | null;
+getSsrc: () => number | null;
+isEnded: () => boolean;
+isLocal: () => true;
+isMuted: () => boolean;
+isReceivingData: () => boolean;
+mute: () => Promise<void>;
+onByteSentStatsReceived: (tpc: any, bytesSent: number) => void; // `tpc` type is not specified
+setConference: (conference: any) => void; // `conference` type is not specified
+setEffect: (effect?: object) => Promise<void>;
+setSourceName: (name: string) => void;
+setSsrc: (ssrc: number) => void;
+stopStream: () => void;
+toString: () => string;
+unmute: () => Promise<void>;
 }
