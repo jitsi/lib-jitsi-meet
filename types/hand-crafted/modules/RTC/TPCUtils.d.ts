4300eda048e059b384752b452121d32595d3f5e1
naman9271 <namanjain9271@gmail.com>
2025-02-17 01:41:12 +0530

    done TPCUtilis.d.ts

--- types/hand-crafted/modules/RTC/TPCUtils.d.ts
+++ types/hand-crafted/modules/RTC/TPCUtils.d.ts
@@ -1,15 +1,98 @@
-import JitsiLocalTrack from './JitsiLocalTrack';
+import JitsiLocalTrack from "./JitsiLocalTrack";
+import { CodecMimeType } from "../../service/RTC/CodecMimeType";
+import { VideoEncoderScalabilityMode } from "../../service/RTC/VideoEncoderScalabilityMode";
+import { MediaType } from "../../service/RTC/MediaType";
+import transform from "sdp-transform";
 
 export default class TPCUtils {
-  constructor(peerconnection: unknown, videoBitrates: unknown); // TODO:
-  ensureCorrectOrderOfSsrcs: ( description: unknown ) => RTCSessionDescription; // TODO:
-  insertUnifiedPlanSimulcastReceive: ( desc: { type: string, sdp: string } ) => RTCSessionDescription;
-  addTrack: ( localTrack: JitsiLocalTrack, isInitiator: boolean ) => void;
-  addTrackUnmute: ( localTrack: JitsiLocalTrack ) => Promise<void>;
-  getLocalStreamHeightConstraints: ( localTrack: JitsiLocalTrack ) => number[];
-  removeTrackMute: ( localTrack: JitsiLocalTrack ) => Promise<void>;
-  replaceTrack: ( oldTrack: JitsiLocalTrack, newTrack: JitsiLocalTrack ) => Promise<void>;
-  setMediaTransferActive: ( active: boolean ) => void;
-  setVideoTransferActive: ( active: boolean ) => void;
-  updateEncodingsResolution: ( parameters: RTCRtpEncodingParameters ) => void;
+    constructor(peerconnection: RTCPeerConnection, videoBitrates: object);
+
+    _calculateActiveEncodingParams: (
+        localVideoTrack: JitsiLocalTrack,
+        codec: CodecMimeType,
+        newHeight: number
+    ) => {
+        active: boolean;
+        maxBitrate: number;
+        scalabilityMode: VideoEncoderScalabilityMode;
+        scaleResolutionDownBy: number;
+    };
+
+    _getConfiguredVideoCodecsImpl: (
+        parsedSdp: transform.SessionDescription
+    ) => Array<CodecMimeType>;
+
+    _getVideoStreamEncodings: (
+        localTrack: JitsiLocalTrack,
+        codec: CodecMimeType
+    ) => Array<object>;
+
+    _isRunningInFullSvcMode: (codec: CodecMimeType) => boolean;
+
+    _isScreenshareBitrateCapped: (localVideoTrack: JitsiLocalTrack) => boolean;
+
+    calculateEncodingsActiveState: (
+        localVideoTrack: JitsiLocalTrack,
+        codec: CodecMimeType,
+        newHeight: number
+    ) => Array<boolean>;
+
+    calculateEncodingsBitrates: (
+        localVideoTrack: JitsiLocalTrack,
+        codec: CodecMimeType,
+        newHeight: number
+    ) => Array<number>;
+
+    calculateEncodingsScalabilityMode: (
+        localVideoTrack: JitsiLocalTrack,
+        codec: CodecMimeType,
+        maxHeight: number
+    ) => Array<VideoEncoderScalabilityMode> | undefined;
+
+    calculateEncodingsScaleFactor: (
+        localVideoTrack: JitsiLocalTrack,
+        codec: CodecMimeType,
+        maxHeight: number
+    ) => Array<number>;
+
+    ensureCorrectOrderOfSsrcs: (
+        description: RTCSessionDescription
+    ) => RTCSessionDescription;
+
+    getConfiguredVideoCodec: () => CodecMimeType;
+
+    getConfiguredVideoCodecs: () => Array<CodecMimeType>;
+
+    getDesiredMediaDirection: () => string;
+
+    getStreamEncodings: () => Array<object>;
+
+    injectSsrcGroupForSimulcast: (sdp: string) => string;
+
+    insertUnifiedPlanSimulcastReceive: (desc: {
+        type: string;
+        sdp: string;
+    }) => RTCSessionDescription;
+
+    isRunningInSimulcastMode: (codec: CodecMimeType) => boolean;
+
+    mungeCodecOrder: (sdp: string) => string;
+
+    mungeOpus: (sdp: string) => string;
+
+    setMaxBitrates: (encodings: Array<object>, maxBitrates: object) => void;
+
+    updateAv1DdHeaders: (sdp: string) => string;
+
+    addTrack: (localTrack: JitsiLocalTrack, isInitiator: boolean) => void;
+    addTrackUnmute: (localTrack: JitsiLocalTrack) => Promise<void>;
+    getLocalStreamHeightConstraints: (localTrack: JitsiLocalTrack) => number[];
+    removeTrackMute: (localTrack: JitsiLocalTrack) => Promise<void>;
+    replaceTrack: (
+        oldTrack: JitsiLocalTrack,
+        newTrack: JitsiLocalTrack
+    ) => Promise<void>;
+    setMediaTransferActive: (active: boolean) => void;
+    setVideoTransferActive: (active: boolean) => void;
+    updateEncodingsResolution: (parameters: RTCRtpEncodingParameters) => void;
 }
---
+++ types/hand-crafted/service/RTC/VideoEncoderScalabilityMode.d.ts
@@ -0,0 +1,13 @@
+export type VideoEncoderScalabilityMode = 
+  | 'L1T1'
+  | 'L1T2'
+  | 'L1T3'
+  | 'L2T1'
+  | 'L2T2'
+  | 'L2T3'
+  | 'L3T1'
+  | 'L3T2'
+  | 'L3T3'
+  | 'None';
+
+export default VideoEncoderScalabilityMode; 
