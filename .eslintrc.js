6e55b846b965d922e30bc98e9f977c933a36e978
naman9271 <namanjain9271@gmail.com>
2025-02-19 16:07:00 +0530

    add rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,12 @@ module.exports = {
         "max-len": ["error", { "code": 120 }], // Limit line length to 120 characters
         "key-spacing": ["error", { "beforeColon": false, "afterColon": true }], // Enforce spacing in object keys
         "func-call-spacing": ["error", "never"], // No space between function name and parentheses
+        "no-console": ["warn", { "allow": ["warn", "error"] }],
+        "no-new": ["error"],
+        "@typescript-eslint/no-import-type-only": ["error"],
+        "logical-assignment-operators": ["error", "always"],
+        "no-unused-private-class-members": ["error"],
+        "no-useless-backreference": ["error"],
+        "require-await": ["error"] 
     }
 };
