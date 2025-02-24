2dc60f47bfeace45fd5a024408d89657f1c5ccdf
naman9271 <namanjain9271@gmail.com>
2025-02-25 03:44:47 +0530

    migrated Jvb121EventGenerator

--- modules/event/Jvb121EventGenerator.ts
+++ modules/event/Jvb121EventGenerator.ts
@@ -1,6 +1,7 @@
 import { getLogger } from '@jitsi/logger';
 
 import * as JitsiConferenceEvents from '../../JitsiConferenceEvents';
+import JitsiConference from '../../JitsiConference';
 
 const logger = getLogger(__filename);
 
@@ -10,11 +11,13 @@ const logger = getLogger(__filename);
  * for more info.
  */
 export default class Jvb121EventGenerator {
+    private _conference: JitsiConference;
+    private _jvb121: boolean;
     /**
      * Creates new <tt>Jvb121EventGenerator</tt> for the given conference.
      * @param {JitsiConference} conference
      */
-    constructor(conference) {
+    constructor(conference:JitsiConference) {
         this._conference = conference;
 
         /**
