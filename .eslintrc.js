e0be5c0522b68bdb7652c981133381696880f892
naman9271 <namanjain9271@gmail.com>
2025-03-04 16:24:29 +0530

    some rules removed

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,9 @@ module.exports = {
         '@jitsi/eslint-config'
     ],
     rules: {
-        "@typescript-eslint/no-import-type-only": ["error"],
-        "logical-assignment-operators": ["error", "always"],
         "no-unused-private-class-members": ["error"],
         "no-useless-backreference": ["error"],
         "require-await": ["error"],
-        "no-floating-promises": ["error"],
         "max-nested-callbacks": ["error", 4]
     }
 };
