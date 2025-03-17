dd36399096992c837d795c4c791042b3fc3deaf9
Saúl Ibarra Corretgé <s@saghul.net>
2025-03-17 13:04:25 +0100

    feat(lint) update to eslint-config-jitsi v6
    It already includes all the necessary dependencies and works for JS and
    TS out of the box.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,5 @@
 module.exports = {
-    parserOptions: {
-        requireConfigFile: false
-    },
-    'extends': [
+    extends: [
         '@jitsi/eslint-config'
-    ],
-    'overrides': [
-        {
-            'files': [ '*.ts' ],
-            extends: [ '@jitsi/eslint-config/typescript' ],
-            parserOptions: {
-                sourceType: 'module',
-                project: [ 'tsconfig.json' ]
-            },
-            rules: {
-                'no-continue': 0
-            }
-        }
     ]
 };
