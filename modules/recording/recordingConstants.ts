92eebc44931e70e0ab88a3cd0e618d8b8464a633
naman9271 <namanjain9271@gmail.com>
2025-03-23 10:54:08 +0530

    migrated

---
+++ modules/recording/recordingConstants.ts
@@ -0,0 +1,41 @@
+interface IRecordingConstants {
+    error: {
+        BUSY: string;
+        ERROR: string;
+        POLICY_VIOLATION: string;
+        RESOURCE_CONSTRAINT: string;
+        UNEXPECTED_REQUEST: string;
+        SERVICE_UNAVAILABLE: string;
+    };
+    mode: {
+        FILE: string;
+        STREAM: string;
+    };
+    status: {
+        OFF: string;
+        ON: string;
+        PENDING: string;
+    };
+}
+
+const recordingConstants: IRecordingConstants = {
+    error: {
+        BUSY: 'busy',
+        ERROR: 'error',
+        POLICY_VIOLATION: 'policy-violation',
+        RESOURCE_CONSTRAINT: 'resource-constraint',
+        UNEXPECTED_REQUEST: 'unexpected-request',
+        SERVICE_UNAVAILABLE: 'service-unavailable'
+    },
+    mode: {
+        FILE: 'file',
+        STREAM: 'stream'
+    },
+    status: {
+        OFF: 'off',
+        ON: 'on',
+        PENDING: 'pending'
+    }
+};
+
+export default recordingConstants;
