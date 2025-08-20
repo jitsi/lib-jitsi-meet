c408d88461ac6fc37ddfe4306fd53fbc8f66ba79
naman9271 <namanjain9271@gmail.com>
2025-08-20 22:45:57 +0530

    migrated

--- modules/proxyconnection/ProxyConnectionService.ts
+++ modules/proxyconnection/ProxyConnectionService.ts
@@ -1,11 +1,14 @@
 import { getLogger } from '@jitsi/logger';
 import { $iq } from 'strophe.js';
 
-import $ from '../../modules/util/XMLParser';
+import JitsiConnection from '../../JitsiConnection';
 import { MediaType } from '../../service/RTC/MediaType';
 import { getSourceNameForJitsiTrack } from '../../service/RTC/SignalingLayer';
 import { VideoType } from '../../service/RTC/VideoType';
+import JitsiLocalTrack from '../RTC/JitsiLocalTrack';
+import JitsiRemoteTrack from '../RTC/JitsiRemoteTrack';
 import RTC from '../RTC/RTC';
+import $ from '../util/XMLParser';
 
 import ProxyConnectionPC from './ProxyConnectionPC';
 import { ACTIONS } from './constants';
@@ -13,11 +16,61 @@ import { ACTIONS } from './constants';
 const logger = getLogger('proxyconnection:ProxyConnectionService');
 
 /**
+ * Interface for ProxyConnectionService constructor options
+ */
+interface IProxyConnectionServiceOptions {
+    convertVideoToDesktop?: boolean;
+    jitsiConnection?: JitsiConnection;
+    onConnectionClosed?: () => void;
+    onRemoteStream?: (track: JitsiLocalTrack) => void;
+    onSendMessage?: (peerJid: string, message: { iq: string; }) => void;
+    pcConfig?: RTCConfiguration;
+}
+
+/**
+ * Interface for internal options with computed values
+ */
+interface IInternalOptions {
+    convertVideoToDesktop?: boolean;
+    onConnectionClosed?: () => void;
+    onRemoteStream?: (track: JitsiLocalTrack) => void;
+    onSendMessage?: (peerJid: string, message: { iq: string; }) => void;
+    pcConfig?: RTCConfiguration;
+}
+
+/**
+ * Interface for message object containing proxy connection information
+ */
+interface IProxyMessage {
+    data: {
+        iq: string;
+    };
+    from: string;
+}
+
+/**
+ * Interface for ProxyConnectionPC creation options
+ */
+interface IProxyConnectionPCOptions {
+    isInitiator?: boolean;
+    receiveAudio?: boolean;
+    receiveVideo?: boolean;
+    pcConfig?: RTCConfiguration;
+    onError?: (peerJid: string, errorType: string, details?: string) => void;
+    onRemoteStream?: (track: JitsiRemoteTrack) => void;
+    onSendMessage?: (peerJid: string, iq: any) => void;
+    peerJid?: string;
+}
+
+/**
  * Instantiates a new ProxyConnectionPC and ensures only one exists at a given
  * time. Currently it assumes ProxyConnectionPC is used only for screensharing
  * and assumes IQs to be used for communication.
  */
 export default class ProxyConnectionService {
+
+    private _options: IInternalOptions;
+    private _peerConnection: Nullable<ProxyConnectionPC>;
     /**
      * Initializes a new {@code ProxyConnectionService} instance.
      *
@@ -32,7 +85,7 @@ export default class ProxyConnectionService {
      * @param {Function} options.onSendMessage - Callback to invoke when a message has to be sent (signaled) out. The
      * arguments passed in are the jid to send the message to and the message.
      */
-    constructor(options = {}) {
+    constructor(options: IProxyConnectionServiceOptions = {}) {
         const {
             jitsiConnection,
             ...otherOptions
@@ -44,7 +97,7 @@ export default class ProxyConnectionService {
          * @type {Object}
          */
         this._options = {
-            pcConfig: jitsiConnection && jitsiConnection.xmpp.connection.jingle.p2pIceConfig,
+            pcConfig: jitsiConnection?.xmpp.connection.jingle.p2pIceConfig,
             ...otherOptions
         };
 
@@ -75,7 +128,7 @@ export default class ProxyConnectionService {
      * sending replies.
      * @returns {void}
      */
-    processMessage(message) {
+    processMessage(message: IProxyMessage): void {
         const peerJid = message.from;
 
         if (!peerJid) {
@@ -97,8 +150,8 @@ export default class ProxyConnectionService {
         }
 
         const iq = this._convertStringToXML(message.data.iq);
-        const $jingle = iq && iq.find('jingle');
-        const action = $jingle && $jingle.attr('action');
+        const $jingle = iq?.find('jingle');
+        const action = $jingle?.attr('action');
 
         if (action === ACTIONS.INITIATE) {
             this._peerConnection = this._createPeerConnection(peerJid, {
@@ -133,7 +186,7 @@ export default class ProxyConnectionService {
      * send through to the peer.
      * @returns {void}
      */
-    start(peerJid, localTracks = []) {
+    start(peerJid: string, localTracks: JitsiLocalTrack[] = []): void {
         this._peerConnection = this._createPeerConnection(peerJid, {
             isInitiator: true,
             receiveVideo: false
@@ -153,7 +206,7 @@ export default class ProxyConnectionService {
      *
      * @returns {void}
      */
-    stop() {
+    stop(): void {
         if (this._peerConnection) {
             this._peerConnection.stop();
         }
@@ -169,7 +222,7 @@ export default class ProxyConnectionService {
      * @returns {Object|null} An element version of the xml. Null will be returned
      * if an error is encountered during transformation.
      */
-    _convertStringToXML(xml) {
+    _convertStringToXML(xml: string): Nullable<ReturnType<typeof $>> {
         try {
             const xmlDom = new DOMParser().parseFromString(xml, 'text/xml');
 
@@ -192,7 +245,7 @@ export default class ProxyConnectionService {
      * @private
      * @returns {ProxyConnectionPC}
      */
-    _createPeerConnection(peerJid, options = {}) {
+    _createPeerConnection(peerJid: string, options: Partial<IProxyConnectionPCOptions> = {}): ProxyConnectionPC {
         if (!peerJid) {
             throw new Error('Cannot create ProxyConnectionPC without a peer.');
         }
@@ -223,7 +276,7 @@ export default class ProxyConnectionService {
      * @private
      * @returns {void}
      */
-    _onFatalError(peerJid, errorType, details = '') {
+    _onFatalError(peerJid: string, errorType: string, details: string = ''): void {
         logger.error(
             'Received a proxy connection error', peerJid, errorType, details);
 
@@ -258,7 +311,7 @@ export default class ProxyConnectionService {
      * @private
      * @returns {void}
      */
-    _onRemoteStream(jitsiRemoteTrack) {
+    _onRemoteStream(jitsiRemoteTrack: JitsiRemoteTrack): void {
         if (!this._options.onRemoteStream) {
             logger.error('Remote track received without callback.');
             jitsiRemoteTrack.dispose();
@@ -267,7 +320,7 @@ export default class ProxyConnectionService {
         }
 
         const isVideo = jitsiRemoteTrack.isVideoTrack();
-        let videoType;
+        let videoType: Optional<VideoType>;
 
         if (isVideo) {
             videoType = this._options.convertVideoToDesktop
@@ -301,7 +354,7 @@ export default class ProxyConnectionService {
      * @private
      * @returns {void}
      */
-    _onSendMessage(peerJid, iq) {
+    _onSendMessage(peerJid: string, iq: any): void {
         if (!this._options.onSendMessage) {
             return;
         }
@@ -322,7 +375,7 @@ export default class ProxyConnectionService {
      * @private
      * @returns {void}
      */
-    _selfCloseConnection() {
+    _selfCloseConnection(): void {
         this.stop();
 
         this._options.onConnectionClosed
